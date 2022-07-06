6c5834e63072b7f09e277c2d686ecf5e01cce046
Ellen Wang <ellen.wang@datadoghq.com>
2022-07-06 13:43:56 -0400

    fix: ignores irrelevant files and directories

--- pysecurity/source_code_analysis/analyzer.py
+++ pysecurity/source_code_analysis/analyzer.py
@@ -3,27 +3,41 @@ from pathlib import Path
 
 from semgrep.semgrep_main import invoke_semgrep
 
+IGNORE = [
+    "helm",
+    "venv",
+    "test",
+    "tests",
+    ".env",
+    "dist",
+    "build",
+    "semgrep",
+    "migrations",
+    ".github",
+    ".semgrep_logs"
+]
 
-def analyze(path, rules=None, prefix=None) -> dict[str]:
-    TARGET_PATH = [Path(path)]
-    RULES_PATH = os.path.join(os.path.dirname(__file__), "semgrep")
-    RULESET = [rule.replace(".yml", "") for rule in os.listdir(RULES_PATH)]
+RULES_PATH = os.path.join(os.path.dirname(__file__), "semgrep")
+RULESET = [rule.replace(".yml", "") for rule in os.listdir(RULES_PATH)]
+
+def analyze(path, rules=None, prefix=None) -> dict[str]:  
+    target_path = [Path(path)]
     
     if rules is None:
-        return get_results(RULESET, RULES_PATH, TARGET_PATH, prefix)
+        return get_results(RULESET, RULES_PATH, target_path, prefix)
     
-    return get_results(rules, RULES_PATH, TARGET_PATH, prefix)
+    return get_results(rules, RULES_PATH, target_path, prefix)
 
 def get_results(rules, rulespath, targetpath, prefix=None):
     results = {rule: {} for rule in rules}
     
     if rules is None:
-        response = invoke_semgrep(Path(os.path.join(rulespath)), targetpath)
+        response = invoke_semgrep(Path(os.path.join(rulespath)), targetpath, exclude = IGNORE)
         return results | format_response(response, prefix=prefix)
     
     for rule in rules:
         try:
-            response = invoke_semgrep(Path(os.path.join(rulespath, rule + ".yml")), targetpath)
+            response = invoke_semgrep(Path(os.path.join(rulespath, rule + ".yml")), targetpath, exclude = IGNORE)
             results = results | format_response(response, rule=rule, prefix=prefix)
         except:
             raise RuntimeError(rule + " is not an existing rule.")
