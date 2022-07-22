668a1bb672a72b37abbebb8ecfaeb787b769da10
Ellen Wang <ellen.wang@datadoghq.com>
2022-07-22 16:38:40 -0400

    fix: Closes #23

--- pysecurity_tests/semgrep/code-execution.py
+++ pysecurity_tests/semgrep/code-execution.py
@@ -52,6 +52,9 @@ def get_version():
 """ OK: os.system used for setup purposes
 """
 # ok: code-execution
+subprocess.check_output(['pkg-config', optional_args, pkg_option, package]).decode('utf8').split()
+
+# ok: code-execution
 os.system('python setup.py sdist bdist_wheel')
 
 # ok: code-execution
