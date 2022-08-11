dcfd4fa101e2fdb7885a5656aefe7fdff01dfd0e
Ellen Wang <ellen.wang@datadoghq.com>
2022-08-11 15:45:33 -0400

    docs: cleaned up typosquatting file

--- guarddog/analyzer/metadata/typosquatting.py
+++ guarddog/analyzer/metadata/typosquatting.py
@@ -17,41 +17,46 @@ from guarddog.analyzer.metadata.detector import Detector
 
 class TyposquatDetector(Detector):
     def __init__(self) -> None:
-        num_packages = 5000
+        # Find top PyPI packages
+        top_packages_information = self._get_top_packages()
+        
+        # Get list of popular packages
+        self.popular_packages = []
+        
+        for package in top_packages_information:
+            name = package["project"]
+            normalized_name = name.lower().replace('_', '-')
+            self.popular_packages.append(normalized_name)
+        
+        super(Detector)
+    
+    
+    def _get_top_packages(self):
         popular_packages_url = "https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.min.json"
         
-        # Fine top PyPI packages
         top_packages_filename = "top_pypi_packages.json"
         resourcesdir = os.path.abspath(os.path.join(os.path.dirname(__file__), "resources"))
         top_packages_path = os.path.join(resourcesdir, top_packages_filename)
         
-        top_package_information = None
+        top_packages_information = None
         
         if top_packages_filename in os.listdir(resourcesdir):
             update_time = datetime.fromtimestamp(os.path.getmtime(top_packages_path))
             
             if datetime.now()-update_time <= timedelta(days=30):
                 top_packages_file = open(top_packages_path, "r")
-                top_package_information = json.load(top_packages_file)["rows"][:num_packages]
+                top_packages_information = json.load(top_packages_file)["rows"]
             
-        if top_package_information is None:           
+        if top_packages_information is None:           
             response = requests.get(popular_packages_url).json()
             with open(top_packages_path, "w+") as f:
                 json.dump(response, f, ensure_ascii=False, indent=4)
                 
-            top_package_information = response["rows"][:num_packages]
-        
-        # Get list of popular packages
-        self.popular_packages = []
-        
-        for package in top_package_information:
-            name = package["project"]
-            normalized_name = name.lower().replace('_', '-')
-            self.popular_packages.append(normalized_name)
-        
-        super(Detector)
+            top_packages_information = response["rows"]
             
-            
+        return top_packages_information
+    
+    
     def _is_distance_one_Levenshtein(self, name1, name2) -> bool:
         if abs(len(name1) - len(name2)) > 1:
             return False
@@ -133,8 +138,6 @@ class TyposquatDetector(Detector):
         
         # Get permuted typosquats for normalized and confused names
         normalized_name = package_name.lower().replace('_', '-')
-        confused_names = self._get_confused_forms(package_name)
-        
         
         # Go through popular packages and find length one edit typosquats
         for popular_package in self.popular_packages:
@@ -152,6 +155,7 @@ class TyposquatDetector(Detector):
             for name in alternate_popular_names + swapped_popular_names:
                 if self._is_length_one_edit_away(normalized_name, name):
                     typosquatted.append(normalized_popular_package)
+                    
         return typosquatted
 
 
