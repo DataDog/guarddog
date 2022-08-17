1072818f01c07e335f133c2a388f335eeaf977e4
Ellen Wang <ellen.wang@datadoghq.com>
2022-08-17 13:06:52 -0400

    docs: documented empty_information

--- guarddog/analyzer/metadata/empty_information.py
+++ guarddog/analyzer/metadata/empty_information.py
@@ -8,9 +8,25 @@ from guarddog.analyzer.metadata.detector import Detector
 
 
 class EmptyInfoDetector(Detector):
+    """
+    Detector for packages with empty information.
+    """
+
     def __init__(self) -> None:
         super(Detector)
 
     def detect(self, package_info) -> bool:
-        sanitized_description = package_info["info"]["description"].split()
+        """
+        Uses a package's information from PyPI's JSON API to determine
+        if the package has an empty description
+
+        Args:
+            package_info (dict): dictionary representation of PyPI's JSON
+                output
+
+        Returns:
+            bool: True if package description is empty
+        """
+
+        sanitized_description = package_info["info"]["description"].strip()
         return len(sanitized_description) == 0
