531b4a44f38a08d1b7f4947a2d16f67c2960fc45
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2022-11-08 08:41:28 +0100

    New rule: detection of common obfuscation methods

---
+++ tests/analyzer/sourcecode/obfuscation.py
@@ -0,0 +1,11 @@
+# Obfuscated with BlankOBF
+# https://github.com/Blank-c/BlankOBF
+
+# ruleid: obfuscation
+_____=eval("\145\166\141\154")
+
+# ruleid: obfuscation
+_____ = eval("foo")
+
+# ok: obfuscation
+eval("foo")
