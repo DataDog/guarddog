a8ac2b64f2b3b78f6efa6aa055933591ebda81c3
vdeturckheim <vlad2t@hotmail.com>
2022-11-30 14:11:52 +0100

    apply review comments

--- tests/core/test_requirements_scanner.py
+++ tests/core/test_requirements_scanner.py
@@ -4,14 +4,14 @@ from guarddog.scanners.project_scanner import RequirementsScanner
 
 
 # Regression test for https://github.com/DataDog/guarddog/issues/78
-def test_requirements_scanner_on_non_existing_package():
+def test_requirements_scanner():
     scanner = RequirementsScanner()
     result = scanner.parse_requirements(["not-a-real-package==1.0.0", "flask==2.2.2"])
-    assert "not-a-real-package" not in result
+    assert "not-a-real-package" not in result  # ignoring non existing packages
     assert "flask" in result
 
 
-# Regression test for
+# Regression test for https://github.com/DataDog/guarddog/issues/88
 def test_requirements_scanner_on_git_url_packages():
     scanner = RequirementsScanner()
     result = scanner.parse_requirements([
