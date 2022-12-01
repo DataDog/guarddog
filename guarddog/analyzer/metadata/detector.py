5279d9053d1b256f3244336714c8369c145326ad
vdeturckheim <vlad2t@hotmail.com>
2022-12-01 10:36:41 +0100

    start integrating type testing

--- guarddog/analyzer/metadata/detector.py
+++ guarddog/analyzer/metadata/detector.py
@@ -1,7 +1,11 @@
+from abc import abstractmethod
+
+
 class Detector:
     def __init__(self) -> None:
         pass
 
     # returns (ruleMatches, message)
+    @abstractmethod
     def detect(self, package_info) -> tuple[bool, str]:
         pass
