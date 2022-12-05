6116c89c4187ac16010708fd0a0f477b30707f3d
vdeturckheim <vlad2t@hotmail.com>
2022-12-05 17:01:54 +0100

    fix tests

--- tests/analyzer/metadata/test_potentially_compromised_email_domain.py
+++ tests/analyzer/metadata/test_potentially_compromised_email_domain.py
@@ -1,17 +1,9 @@
-import json
-import os
-import pathlib
 from datetime import datetime
 
-import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
-from guarddog.analyzer.metadata.npm import NPMPotentiallyCompromisedEmailDomainDetector
-from guarddog.analyzer.metadata.pypi import PypiPotentiallyCompromisedEmailDomainDetector
+from guarddog.analyzer.metadata.potentially_compromised_email_domain import PotentiallyCompromisedEmailDomainDetector
 from tests.analyzer.metadata.resources.sample_project_info import PACKAGE_INFO
-
-with open(os.path.join(pathlib.Path(__file__).parent.resolve(), "resources", "npm_data.json"), "r") as file:
-    NPM_PACKAGE_INFO = json.load(file)
 
 
 class MockWhoIs:
@@ -19,28 +11,23 @@ class MockWhoIs:
         self.creation_date = date
 
 
-pypi_detector = PypiPotentiallyCompromisedEmailDomainDetector()
-npm_detector = NPMPotentiallyCompromisedEmailDomainDetector()
-
-
 class TestCompromisedEmail:
+    detector = PotentiallyCompromisedEmailDomainDetector()
 
-    @pytest.mark.parametrize("package_info, detector", [(PACKAGE_INFO, pypi_detector), (NPM_PACKAGE_INFO, npm_detector)])
-    def test_compromised(self, package_info, detector):
+    def test_compromised(self):
         def mock_whois(domain):
             return MockWhoIs(datetime.today())
 
         MonkeyPatch().setattr("whois.whois", mock_whois)
-        compromised, _ = detector.detect(package_info)
+        compromised, _ = self.detector.detect(PACKAGE_INFO, "pypi")
         assert compromised
 
-    @pytest.mark.parametrize("package_info, detector", [(PACKAGE_INFO, pypi_detector), (NPM_PACKAGE_INFO, npm_detector)])
-    def test_safe(self, package_info, detector):
+    def test_safe(self):
         def mock_whois(domain):
             return MockWhoIs(datetime(1990, 1, 31))
 
         MonkeyPatch().setattr("whois.whois", mock_whois)
-        compromised, _ = detector.detect(package_info)
+        compromised, _ = self.detector.detect(PACKAGE_INFO, "pypi")
         assert not compromised
 
     def test_email_domain_doesnt_exist(self):
@@ -49,5 +36,5 @@ class TestCompromisedEmail:
             raise whois.parser.PywhoisError('No match for "nope.com".')
 
         MonkeyPatch().setattr("whois.whois", mock_whois)
-        compromised, _ = pypi_detector.detect(PACKAGE_INFO)
+        compromised, _ = self.detector.detect(PACKAGE_INFO, "pypi")
         assert compromised
