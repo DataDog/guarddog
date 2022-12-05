9b1fc83d72370aaf07a2fb89717aa7287af22333
vdeturckheim <vlad2t@hotmail.com>
2022-12-05 13:50:48 +0100

    move release zero heuritics

--- guarddog/analyzer/metadata/release_zero.py
+++ guarddog/analyzer/metadata/release_zero.py
@@ -10,5 +10,11 @@ from guarddog.analyzer.metadata.detector import Detector
 class ReleaseZeroDetector(Detector):
 
     def detect(self, package_info, ecosystem: str) -> tuple[bool, str]:
-        return (package_info["info"]['version'] in ['0.0.0', '0.0'],
-                "The package has its latest release version to 0.0.0")
+        if ecosystem == 'pypi':
+            return (package_info["info"]["version"] in ["0.0.0", "0.0"],
+                    "The package has its latest release version to 0.0.0")
+        if ecosystem == 'npm':
+            if package_info["dist-tags"]["latest"] in ["0.0.0", "0.0", "0"]:
+                return True, "The package has its latest release version to 0.0.0"
+            return False, ""
+        raise NotImplementedError(f"Not implemented for ecosystem {ecosystem}")
