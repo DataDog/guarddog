25f5cb6705ad91e844ca76e12d42de659fcfaea6
vdeturckheim <vlad2t@hotmail.com>
2022-12-08 16:01:41 +0100

    add tests

--- tests/core/test_npm_requirements_scanner.py
+++ tests/core/test_npm_requirements_scanner.py
@@ -1,3 +1,4 @@
+
 from guarddog.scanners.npm_project_scanner import NPMRequirementsScanner
 
 
@@ -9,23 +10,8 @@ def test_npm_requirements_scanner():
             "non-existing": "*",
             "express": "4.x"
         }
-    }
-    """)
+    }""")
     assert "non-existing" not in result  # ignoring non existing packages
     assert "express" in result
-
-
-def test_npm_requirements_scanner_github():
-    scanner = NPMRequirementsScanner()
-    result = scanner.parse_requirements("""
-    {
-        "dependencies": {
-            "express": "expressjs/express",
-            "cors": "https://github.com/expressjs/cors.git"
-        }
-    }
-    """)
-    assert "express" in result
-    assert "expressjs/express" in result["express"]
-    assert "cors" in result
-    assert "https://github.com/expressjs/cors.git" in result["cors"]
+# TODO: handle non-version cases
+# TODO: test also the package scanner
