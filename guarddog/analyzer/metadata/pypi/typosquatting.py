e9e12dc1fbbef324319f10a0ac660be51c6ef94c
vdeturckheim <vlad2t@hotmail.com>
2022-12-12 16:39:04 +0100

    fix types

--- guarddog/analyzer/metadata/pypi/typosquatting.py
+++ guarddog/analyzer/metadata/pypi/typosquatting.py
@@ -13,7 +13,7 @@ class PypiTyposquatDetector(TyposquatDetector):
         popular_packages (list): list of top 5000 downloaded packages from PyPI
     """
 
-    def detect(self, package_info) -> tuple[bool, Optional[str]]:
+    def detect(self, package_info, path: Optional[str] = None) -> tuple[bool, Optional[str]]:
         """
         Uses a package's information from PyPI's JSON API to determine the
         package is attempting a typosquatting attack
@@ -25,6 +25,7 @@ class PypiTyposquatDetector(TyposquatDetector):
         Returns:
             list[str]: names of packages that <package_name> could be
             typosquatting from
+            @param **kwargs:
         """
         similar_package_names = self.get_typosquatted_package(package_info["info"]["name"])
         if len(similar_package_names) > 0:
