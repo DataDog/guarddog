3e165fa68daeb928895c3fbc07b3ecb06e80d952
vdeturckheim <vlad2t@hotmail.com>
2022-12-19 10:59:00 +0100

    save commit

--- guarddog/analyzer/metadata/pypi/repository_integrity_missmatch.py
+++ guarddog/analyzer/metadata/pypi/repository_integrity_missmatch.py
@@ -2,20 +2,25 @@
 
 Detects if a package contains an empty description
 """
+import os
 import re
 from typing import Optional
+
+import requests
 
 from guarddog.analyzer.metadata.repository_integrity_missmatch import IntegrityMissmatch
 
 
 GH_REPO_REGEX = r'(?:https?://)?(?:www\.)?github\.com/(?:[\w-]+/){2}'
+GH_REPO_OWNER_REGEX = r'(?:https?://)?(?:www\.)?github\.com/([\w-]+)/([\w-]+)'
 
-
-def find_repository_url(string):
-    match = re.search(GH_REPO_REGEX, string)
+def extract_owner_and_repo(url):
+    match = re.search(GH_REPO_OWNER_REGEX, url)
     if match:
-        return match.group(0)
-    return None
+        owner = match.group(1)
+        repo = match.group(2)
+        return owner, repo
+    return None, None
 
 
 def dict_generator(indict, pre=None):
@@ -44,8 +49,8 @@ class PypiIntegrityMissmatch(IntegrityMissmatch):
         # let's extract a source repository (GitHub only for now) if we can
         infos = package_info["info"]
         github_urls = set()
-        for path in dict_generator(infos):
-            leaf = path[-1]
+        for dict_path in dict_generator(infos):
+            leaf = dict_path[-1]
             if type(leaf) is not str:
                 continue
             res = re.findall(GH_REPO_REGEX, leaf)
@@ -55,9 +60,29 @@ class PypiIntegrityMissmatch(IntegrityMissmatch):
 
         if len(github_urls) == 0:
             return False, "Could not find any GitHub url in the project's description"
-        # now
+        # now, let's find the right url
+        best_candidate = None
+        for entry in github_urls:
+            if f"/{name}" in entry:
+                best_candidate = entry
+                break
+
+        if best_candidate is None:
+            return False, "Could not find a good GitHub url in the project's description"
 
         # ok, now let's try to find the version! (I need to know which version we are scanning)
+        if version is None:
+            return False, ""  # FIXME: use latest come on!
+        tmp_dir = os.path.dirname(path)
+
+        owner, repo = extract_owner_and_repo(best_candidate)
+        if owner is None or repo is None:
+            raise Exception(f"Could not parse url {best_candidate}")
+
+        tags = requests.get(f"https://api.github.com/repos/{owner}/{repo}/tags")
+        print(tags)
+
+
         # with a GH release/tag
         # by finding the commit setting the version
         # cool we have the version, let's compare files that exist
