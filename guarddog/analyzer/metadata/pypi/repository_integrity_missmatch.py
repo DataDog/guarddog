8a768b8ac3bc9d6a6a56d97437537de58fc99f68
vdeturckheim <vlad2t@hotmail.com>
2022-12-20 17:28:51 +0100

    highlight last issue

--- guarddog/analyzer/metadata/pypi/repository_integrity_missmatch.py
+++ guarddog/analyzer/metadata/pypi/repository_integrity_missmatch.py
@@ -7,7 +7,7 @@ import difflib
 import hashlib
 import os
 import re
-from typing import Optional
+from typing import Optional, Set, Any
 
 import pygit2
 from semantic_version import Version
@@ -21,9 +21,25 @@ GH_REPO_OWNER_REGEX = r'(?:https?://)?(?:www\.)?github\.com/([\w-]+)/([\w-]+)'
 # eventually, GuardDog core should be doing the job of selecting a single version to scan
 def parse_version(raw):
     try:
-        return Version.coerce(raw)
+        v = Version.coerce(raw)
+        return {
+            "version": v,
+            "raw": raw
+        }
     except Exception:
-        return Version("0.0.0")
+        return {
+            "version": Version("0.0.0"),
+            "raw": raw
+        }
+
+
+def find_version(package_info):
+    all_versions = list(map(
+        parse_version,
+        list(package_info["releases"].keys())
+    ))
+    version = max(all_versions, key=lambda x: x["version"])
+    return version["raw"]
 
 
 def extract_owner_and_repo(url):
@@ -64,6 +80,65 @@ def get_file_hash(path):
         return hash_object.hexdigest(), str(file_contents).strip().splitlines()
 
 
+def find_github_candidates(package_info) -> set[str]:
+    infos = package_info["info"]
+    github_urls = set()
+    for dict_path in dict_generator(infos):
+        leaf = dict_path[-1]
+        if type(leaf) is not str:
+            continue
+        res = re.findall(GH_REPO_REGEX, leaf)
+        if len(res) > 0:
+            for cd in res:
+                github_urls.add(cd.strip())
+    return github_urls
+
+
+def exclude_result(file_name, repo_root, pkg_root):
+    if file_name.endswith('.rst') or file_name.endswith('.md'):
+        return True
+    if file_name.endswith("setup.cfg"):
+        repo_cfg = configparser.ConfigParser()
+        repo_cfg.read(os.path.join(repo_root, file_name))
+        pkg_cfg = configparser.ConfigParser()
+        pkg_cfg.read(os.path.join(pkg_root, file_name))
+        repo_sections = list(repo_cfg.keys())
+        pkg_sections = list(pkg_cfg.keys())
+        if "egg_info" in pkg_sections and "egg_info" not in repo_sections:
+            return True
+    return False
+
+
+def find_missmatch_for_tag(repo, tag, base_path, repo_path):
+    repo.checkout(tag)
+    missmatch = []
+    for root, dirs, files in os.walk(base_path):
+        relative_path = os.path.relpath(root, base_path)
+        repo_root = os.path.join(repo_path, relative_path)
+        if not os.path.exists(repo_root):
+            continue
+        repo_files = list(filter(
+            lambda x: os.path.isfile(os.path.join(repo_root, x)),
+            os.listdir(repo_root)
+        ))
+        for file_name in repo_files:
+            if file_name not in files:  # ignore files we don't have in the distribution
+                continue
+            repo_hash, repo_content = get_file_hash(os.path.join(repo_root, file_name))
+            pkg_hash, pkg_content = get_file_hash(os.path.join(root, file_name))
+            if repo_hash != pkg_hash:
+                if exclude_result(file_name, repo_root, root):
+                    continue
+                res = {
+                    "file": os.path.join(relative_path, file_name),
+                    "repo_sha256": repo_hash,
+                    "pkg_sha256": pkg_hash
+                }
+                missmatch.append(res)
+    return missmatch
+
+
+# Note: we should have the GitHub related logic factored out as we will need it when we check for signed commits
 class PypiIntegrityMissmatch(IntegrityMissmatch):
     """This package contains files that have been tampered with between the source repository and the package CDN"""
     RULE_NAME = "repository_integrity_missmatch"
@@ -71,17 +146,7 @@ class PypiIntegrityMissmatch(IntegrityMissmatch):
     def detect(self, package_info, path: Optional[str] = None, name: Optional[str] = None,
                version: Optional[str] = None) -> tuple[bool, str]:
         # let's extract a source repository (GitHub only for now) if we can
-        infos = package_info["info"]
-        github_urls = set()
-        for dict_path in dict_generator(infos):
-            leaf = dict_path[-1]
-            if type(leaf) is not str:
-                continue
-            res = re.findall(GH_REPO_REGEX, leaf)
-            if len(res) > 0:
-                for cd in res:
-                    github_urls.add(cd.strip())
-
+        github_urls = find_github_candidates(package_info)
         if len(github_urls) == 0:
             return False, "Could not find any GitHub url in the project's description"
         # now, let's find the right url
@@ -96,11 +161,9 @@ class PypiIntegrityMissmatch(IntegrityMissmatch):
 
         # ok, now let's try to find the version! (I need to know which version we are scanning)
         if version is None:
-            all_versions = list(map(
-                parse_version,
-                list(package_info["releases"].keys())
-            ))
-            version = str(max(all_versions))
+            version = find_version(package_info)
+        if version is None:
+            raise Exception("Could not find suitable version to scan")
         tmp_dir = os.path.dirname(path)
 
         owner, repo = extract_owner_and_repo(github_url)
@@ -121,70 +184,32 @@ class PypiIntegrityMissmatch(IntegrityMissmatch):
             if version in tag_name:
                 tag_candidates.append(tag_name)
 
-        #  TODO: parse the code of the package to find the real real version
-        # Idea: we can grep the project files for the version, the, git biscect until we have a file with the same version?
-        # will not work if main has not been bumped yet in version
-        # so tags and releases are out only solutions here
-        # print(tag_candidates)
+        # TODO: parse the code of the package to find the real real version Idea: we can grep the project files for
+        #  the version, the, git biscect until we have a file with the same version? will not work if main has not
+        #  been bumped yet in version so tags and releases are out only solutions here print(tag_candidates)
+        #  Well, that works if we run integrity check for multiple commits
+
+        #  should be good, but let's open the sources
+        base_dir_name = None
+        for entry in os.listdir(path):
+            if entry.lower().startswith(name.lower()):
+                base_dir_name = entry
+        if base_dir_name is None or base_dir_name == "sources":  # I am not sure how we can get there
+            raise Exception("something went wrong when opening the package")
+        base_path = os.path.join(path, base_dir_name)
+
+        if len(tag_candidates) == 0:
+            return False, "Could not find any suitable tag in repository"
 
         target_tag = None
         # best candidate should have the closest
-        for tag in tag_candidates:  # FIXME
+        for tag in tag_candidates:  # FIXME: for pyaml we scan 6.0 instead of 6.0b1 - there must be a way
             target_tag = tag
 
-        if target_tag is None:
-            return False, "Could not find a suitable tag in repository"
-        repo.checkout(target_tag)
-        # with a GH release/tag
-        # by finding the commit setting the version
-        # cool we have the version, let's compare files that exist
-        base_dir_name = os.listdir(path)[0]  # FIXME!!
-        base_path = os.path.join(path, base_dir_name)
-        missmatch = []
-        for root, dirs, files in os.walk(base_path):
-            relative_path = os.path.relpath(root, base_path)
-            repo_root = os.path.join(repo_path, relative_path)
-            if not os.path.exists(repo_root):
-                continue
-            repo_files = list(filter(
-                lambda x: os.path.isfile(os.path.join(repo_root, x)),
-                os.listdir(repo_root)
-            ))
-            for file_name in repo_files:
-                if file_name not in files:
-                    continue
-                repo_hash, repo_content = get_file_hash(os.path.join(repo_root, file_name))
-                pkg_hash, pkg_content = get_file_hash(os.path.join(root, file_name))
-                if repo_hash != pkg_hash:
-                    if file_name.endswith('.rst') or file_name.endswith('.md'):
-                        continue  # let's ignore non executable files
-                    if file_name.endswith("setup.cfg"):
-                        repo_cfg = configparser.ConfigParser()
-                        repo_cfg.read(os.path.join(repo_root, file_name))
-                        pkg_cfg = configparser.ConfigParser()
-                        pkg_cfg.read(os.path.join(root, file_name))
-                        repo_sections = list(repo_cfg.keys())
-                        pkg_sections = list(pkg_cfg.keys())
-                        if "egg_info" in pkg_sections and "egg_info" not in repo_sections:
-                            continue
-                    res = {
-                        "file": os.path.join(relative_path, file_name),
-                        "repo_sha256": repo_hash,
-                        "pkg_sha256": pkg_hash,
-                        # This does not work in terms of output yet
-                        # "diff": "\n".join(
-                        #     list(
-                        #         difflib.unified_diff(repo_content,
-                        #                              pkg_content,
-                        #                              fromfile=os.path.join("sources", file_name),
-                        #                              tofile=os.path.join("package", file_name),
-                        #                              lineterm=''))
-                        # )
-                    }
-                    missmatch.append(res)
+        missmatch = find_missmatch_for_tag(repo, target_tag, base_path, repo_path)
         message = "\n".join(map(
-            lambda x: x["file"],
+            lambda x: "* " + x["file"],
             missmatch
         ))
         return len(missmatch) > 0, f"Some files present in the package are different from the ones on GitHub for " \
-                                   f"the same version of the package: {message}"
+                                   f"the same version of the package: \n{message}"
