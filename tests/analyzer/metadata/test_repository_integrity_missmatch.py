f0af13d4cd0e30938a54a9cbd9a59cc1102d9594
vdeturckheim <vlad2t@hotmail.com>
2022-12-22 15:50:33 +0100

    fix tests

--- tests/analyzer/metadata/test_repository_integrity_missmatch.py
+++ tests/analyzer/metadata/test_repository_integrity_missmatch.py
@@ -1,9 +1,4 @@
-import json
-import os
-import pathlib
 from copy import deepcopy
-
-import pytest
 
 from guarddog.analyzer.metadata.pypi import PypiIntegrityMissmatch
 from tests.analyzer.metadata.resources.sample_project_info import PACKAGE_INFO
@@ -14,7 +9,7 @@ def test_no_github_links():
     current_info["info"]["home_page"] = ""
     current_info["info"]["project_urls"]["Homepage"] = ""
     detector = PypiIntegrityMissmatch()
-    match, message = detector.detect(current_info)
+    match, message = detector.detect(current_info, name="", path="")
     assert not match
     assert message == "Could not find any GitHub url in the project's description"
 
@@ -25,6 +20,6 @@ def test_no_good_github_links():
     current_info["info"]["project_urls"]["Homepage"] = ""
     current_info["info"]["summary"] = "https://github.com/pypa/sampleproject"
     detector = PypiIntegrityMissmatch()
-    match, message = detector.detect(current_info, name="mypackage")
+    match, message = detector.detect(current_info, name="mypackage", path="")
     assert not match
     assert message == "Could not find a good GitHub url in the project's description"
