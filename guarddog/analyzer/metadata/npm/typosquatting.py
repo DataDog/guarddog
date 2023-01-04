6bcb9832d5b8e03106472b9f9dfea884e237e7b0
vdeturckheim <vlad2t@hotmail.com>
2023-01-04 15:43:01 +0100

    started to add npm typposquatter

--- guarddog/analyzer/metadata/npm/typosquatting.py
+++ guarddog/analyzer/metadata/npm/typosquatting.py
@@ -1,15 +1,11 @@
 import json
 import os
-from datetime import datetime, timedelta
 from typing import Optional
-
-import requests
-from packaging.utils import canonicalize_name
 
 from guarddog.analyzer.metadata.typosquatting import TyposquatDetector
 
 
-class PypiTyposquatDetector(TyposquatDetector):
+class NPMTyposquatDetector(TyposquatDetector):
     """
     Detector for typosquatting attacks. Detects if a package name is a typosquat of one of the top 1000 packages.
     Checks for distance one Levenshtein, one-off character swaps, permutations
@@ -35,32 +31,12 @@ class PypiTyposquatDetector(TyposquatDetector):
                 ]
         """
 
-        popular_packages_url = "https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.min.json"
-
-        top_packages_filename = "top_pypi_packages.json"
+        top_packages_filename = "top_npm_packages.json"
         resources_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "resources"))
-        top_packages_path = os.path.join(resources_dir, top_packages_filename)
+        with open(os.path.join(resources_dir, top_packages_filename)) as file:
+            top_packages_data = json.load(file)
 
-        top_packages_information = None
-
-        if top_packages_filename in os.listdir(resources_dir):
-            update_time = datetime.fromtimestamp(os.path.getmtime(top_packages_path))
-
-            if datetime.now() - update_time <= timedelta(days=30):
-                top_packages_file = open(top_packages_path, "r")
-                top_packages_information = json.load(top_packages_file)["rows"]
-
-        if top_packages_information is None:
-            response = requests.get(popular_packages_url).json()
-            with open(top_packages_path, "w+") as f:
-                json.dump(response, f, ensure_ascii=False, indent=4)
-
-            top_packages_information = response["rows"]
-
-        def get_safe_name(package):
-            return canonicalize_name(package["project"])
-
-        return list(map(get_safe_name, top_packages_information))
+        return list(map(lambda x: x["project"], top_packages_data))
 
     def detect(self, package_info, path: Optional[str] = None) -> tuple[bool, Optional[str]]:
         """
