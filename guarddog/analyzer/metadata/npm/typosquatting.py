97249048bbec46a535dfe76c1e5b8582c45d2349
vdeturckheim <vlad2t@hotmail.com>
2023-01-04 17:07:31 +0100

    update doc and add tests

--- guarddog/analyzer/metadata/npm/typosquatting.py
+++ guarddog/analyzer/metadata/npm/typosquatting.py
@@ -6,31 +6,15 @@ from guarddog.analyzer.metadata.typosquatting import TyposquatDetector
 
 
 class NPMTyposquatDetector(TyposquatDetector):
-    """
-    Detector for typosquatting attacks. Detects if a package name is a typosquat of one of the top 1000 packages.
+    """Detector for typosquatting attacks. Detects if a package name is a typosquat of one of the top 1000 packages.
     Checks for distance one Levenshtein, one-off character swaps, permutations
     around hyphens, and substrings.
 
     Attributes:
-        popular_packages (list): list of top 5000 downloaded packages from PyPI
+        popular_packages (list): list of top 5000 downloaded packages from npm
     """
 
     def _get_top_packages(self) -> list:
-        """
-        Gets the package information of the top 5000 most downloaded PyPI packages
-
-        Returns:
-            list: list of package data in the format:
-                [
-                    ...
-                    {
-                        download_count: ...
-                        project: <package-name>
-                    }
-                    ...
-                ]
-        """
-
         top_packages_filename = "top_npm_packages.json"
         resources_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "resources"))
         with open(os.path.join(resources_dir, top_packages_filename)) as file:
