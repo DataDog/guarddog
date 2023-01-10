81feb7201c137d03304a2968adc5b76516f203c3
Vladimir de Turckheim <vdeturckheim@users.noreply.github.com>
2023-01-10 10:22:52 +0100

    npm squatting (#121)
    * started to add npm typposquatter
    
    * update doc and add tests
    
    * update comment
    
    * fix typposquatting

--- tests/analyzer/metadata/test_typosquatting.py
+++ tests/analyzer/metadata/test_typosquatting.py
@@ -1,12 +1,14 @@
 import pytest
 
+from guarddog.analyzer.metadata.npm import NPMTyposquatDetector
 from guarddog.analyzer.metadata.pypi import PypiTyposquatDetector
 from tests.analyzer.metadata.resources.sample_project_info import generate_project_info
 
 
 class TestTyposquatting:
-    detector = PypiTyposquatDetector()
-    typosquats = [
+    pypi_detector = PypiTyposquatDetector()
+    npm_detector = NPMTyposquatDetector()
+    pypi_typosquats = [
         ("ans1crypto", "asn1crypto"),
         ("colourama", "colorama"),
         ("djanga", "django"),
@@ -28,19 +30,34 @@ class TestTyposquatting:
         ("virtualnv", "virtualenv"),
     ]
 
+    npm_typosquats = [
+        ("expresss", "express"),
+        ("wich-boxed-primitive", "which-boxed-primitive"),
+        ("twetnacl", "tweetnacl"),
+        ("jest-watchers", "jest-watcher"),
+        ("shpk", "sshpk"),
+        ("requests", "request")
+    ]
+
     negative_cases = ["hello-world", "foo", "bar"]
     same_names = ["pip", "Numpy", "openCv-python", "requests_oauthlib"]
 
-    @pytest.mark.parametrize("typo_name, real_name", typosquats)
-    def test_typosquats(self, typo_name, real_name):
+    @pytest.mark.parametrize("typo_name, real_name", pypi_typosquats)
+    def test_pypi_typosquats(self, typo_name, real_name):
         project_info = generate_project_info("name", typo_name)
-        matches, message = self.detector.detect(project_info)
+        matches, message = self.pypi_detector.detect(project_info)
+        assert matches and real_name in message
+
+    @pytest.mark.parametrize("typo_name, real_name", npm_typosquats)
+    def test_npm_typosquats(self, typo_name, real_name):
+        project_info = {"name": typo_name}
+        matches, message = self.npm_detector.detect(project_info)
         assert matches and real_name in message
 
     @pytest.mark.parametrize("name", negative_cases + same_names)
     def test_nontyposquats(self, name):
         project_info = generate_project_info("name", name)
-        matches, _ = self.detector.detect(project_info)
+        matches, _ = self.pypi_detector.detect(project_info)
         assert not matches
 
     def test_no_duplicate_errors(self):
@@ -49,7 +66,7 @@ class TestTyposquatting:
 
         Regression test for https://github.com/DataDog/guarddog/issues/71
         """
-        result = self.detector.get_typosquatted_package("pdfminer.sid")
+        result = self.pypi_detector.get_typosquatted_package("pdfminer.sid")
         assert len(result) == 1
 
     def test_normalize_names(self):
@@ -59,5 +76,5 @@ class TestTyposquatting:
 
         Regression test for https://github.com/DataDog/guarddog/issues/71
         """
-        result = self.detector.get_typosquatted_package("pdfminer...---___six")
+        result = self.pypi_detector.get_typosquatted_package("pdfminer...---___six")
         assert len(result) == 0
