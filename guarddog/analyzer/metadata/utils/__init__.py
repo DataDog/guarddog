e64e642ee96fdf9868092cea2cfd058c186f9829
vdeturckheim <vlad2t@hotmail.com>
2023-01-17 16:03:55 +0100

    start factoring out finding repo logic

--- guarddog/analyzer/metadata/utils/__init__.py
+++ guarddog/analyzer/metadata/utils/__init__.py
@@ -4,13 +4,20 @@ from guarddog.ecosystems import ECOSYSTEM
 
 class UtilBundle:
 
-    def __init__(self, repository_cloner_class=None):
+    def __init__(self, ecosystem, repository_cloner_class=None):
+        self.ecosystem = ecosystem  # I am here for debugging reasons
         self.repository_cloner_class = repository_cloner_class
 
 
 def get_util_bundle(ecosystem: ECOSYSTEM):
     match ecosystem:
         case ECOSYSTEM.PYPI:
-            return UtilBundle(PypiRepositoryCloner)
+            return UtilBundle(
+                ecosystem,
+                repository_cloner_class=PypiRepositoryCloner
+            )
         case ECOSYSTEM.NPM:
-            return UtilBundle()
+            return UtilBundle(
+                ecosystem,
+                repository_cloner_class=None
+            )
