3ab9f131b93641267633bca261c9b5f5e8a8a427
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2023-02-09 14:04:02 +0100

    Fix bug on local target

--- tests/core/test_cli.py
+++ tests/core/test_cli.py
@@ -7,6 +7,7 @@ import guarddog.cli
 def test_is_local_target():
     assert guarddog.cli.is_local_target("/tmp/foo")
     assert guarddog.cli.is_local_target("./foo")
+    assert guarddog.cli.is_local_target("../foo")
     assert not guarddog.cli.is_local_target("foo")
 
     with unittest.mock.patch('os.path.exists') as mock:
