7843a042e76271477030f4324e4e0899a72574a8
Christophe Tafani-Dereeper <christophe.tafanidereeper@datadoghq.com>
2023-03-08 10:46:00 +0100

    Add unit test

--- tests/analyzer/metadata/test_repository_integrity_mismatch.py
+++ tests/analyzer/metadata/test_repository_integrity_mismatch.py
@@ -23,3 +23,14 @@ def test_no_good_github_links():
     match, message = detector.detect(current_info, name="mypackage", path="")
     assert not match
     assert message == "Could not find a good GitHub url in the project's description"
+
+
+def test_empty_homepage_urls():
+    """
+    Regression test for https://github.com/DataDog/guarddog/issues/190
+    """
+    current_info = deepcopy(PYPI_PACKAGE_INFO)
+    current_info["info"]["project_urls"] = None
+    detector = PypiIntegrityMismatchDetector()
+    match, _ = detector.detect(current_info, name="mypackage", path="")
+    assert not match
