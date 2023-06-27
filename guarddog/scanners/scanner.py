385e1376b66e809cf967b1a4003099a3eab5a379
H4dr1en <h4dr1en@pm.me>
2023-06-27 09:10:59 +0200

    add support for pipenv

--- guarddog/scanners/scanner.py
+++ guarddog/scanners/scanner.py
@@ -236,9 +236,10 @@ class PackageScanner(Scanner):
                     return self.analyzer.analyze_sourcecode(tmpdirname, rules=rules)
             elif os.path.isdir(path):
                 return self.analyzer.analyze_sourcecode(path, rules=rules)
-            else:
-                raise Exception(f"Path {path} is not a directory nor an archive type supported by GuardDog.")
-        raise Exception(f"Path {path} does not exist.")
+            else:  # is a file
+                return self.analyzer.analyze_sourcecode(path, rules=rules)
+        else:
+            raise Exception(f"Path {path} does not exist.")
 
     @abstractmethod
     def download_and_get_package_info(self, directory: str, package_name: str, version=None) -> typing.Tuple[dict, str]:
