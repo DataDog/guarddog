3779edfffa55e0f01da8a4fbb58cdfbd8fb5970f
Claire Thibodeaux <claire.thibodeaux@datadoghq.com>
2023-07-19 15:45:34 -0400

    added none check to get_email_addresses pypi

--- guarddog/analyzer/metadata/pypi/potentially_compromised_email_domain.py
+++ guarddog/analyzer/metadata/pypi/potentially_compromised_email_domain.py
@@ -19,7 +19,10 @@ class PypiPotentiallyCompromisedEmailDomainDetector(PotentiallyCompromisedEmailD
         author_email = package_info["info"]["author_email"]
         maintainer_email = package_info["info"]["maintainer_email"]
         email = author_email or maintainer_email
-        return [email]
+        if email is not None:
+            return [email]
+        else:
+            return []
 
     def get_project_latest_release_date(self, package_info) -> Optional[datetime]:
         """
