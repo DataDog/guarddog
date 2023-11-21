64ea04069894e0b117e0187156a4b75bceb2731f
Julien Doutre <julien.doutre@datadoghq.com>
2023-11-21 13:23:23 +0100

    Fmt and lint

--- guarddog/analyzer/metadata/npm/npm_shrinkwrap_file.py
+++ guarddog/analyzer/metadata/npm/npm_shrinkwrap_file.py
@@ -3,20 +3,22 @@ from pathlib import Path
 
 from guarddog.analyzer.metadata.detector import Detector
 
+
 class NPMShrinkwrapFile(Detector):
     def __init__(self):
         super().__init__(
             name="npm_shrinkwrap_file",
-            description="Identify packages with a npm-shrinkwrap.json file which prevents end users from updating transitive dependencies."
+            description="Identify packages with a npm-shrinkwrap.json file \
+which prevents end users from updating transitive dependencies.",
         )
 
     def detect(
-            self,
-            package_info,
-            path: Optional[str] = None,
-            name: Optional[str] = None,
-            version: Optional[str] = None,
-            ) -> tuple[bool, Optional[str]]:
+        self,
+        package_info,
+        path: Optional[str] = None,
+        name: Optional[str] = None,
+        version: Optional[str] = None,
+    ) -> tuple[bool, Optional[str]]:
         if path is None:
             raise ValueError("path is needed to run heuristic " + self.get_name())
 
