65157d394ae38856f05f5520913d5172094f7eb9
Julien Doutre <julien.doutre@datadoghq.com>
2024-04-11 16:58:15 +0200

    Linting, typing and main field

--- guarddog/analyzer/metadata/npm/npm_metadata_mismatch.py
+++ guarddog/analyzer/metadata/npm/npm_metadata_mismatch.py
@@ -1,15 +1,15 @@
-from typing import Optional, Any
+from typing import Optional, Any, Dict
 from pathlib import Path
 import json
 
 from guarddog.analyzer.metadata.detector import Detector
 
-# List of fields where mismatch between package.json and NPM can carry malicious information
-# (field, expected type)
+# List of fields where mismatch between package.json and NPM can carry malicious information.
 MANIFEST_FIELDS_CHECKLIST = {
-    "scripts": dict,
     "dependencies": dict,
     "devDependencies": dict,
+    "scripts": dict,
+    "main": str,
 }
 
 
@@ -17,11 +17,17 @@ class NPMMetadataMismatch(Detector):
     def __init__(self):
         super().__init__(
             name="npm_metadata_mismatch",
-            description="Identify packages which have mismatches between the npm package manifest and the package info for some critical fields"
+            description="Identify packages which have mismatches between the npm package"
+            "manifest and the package info for some critical fields",
         )
 
-    def detect(self, package_info, path: Optional[str] = None, name: Optional[str] = None,
-               version: Optional[str] = None) -> tuple[bool, Optional[str]]:
+    def detect(
+        self,
+        package_info,
+        path: Optional[str] = None,
+        name: Optional[str] = None,
+        version: Optional[str] = None,
+    ) -> tuple[bool, Optional[str]]:
         # Get the latest version if not specified
         if not version:
             version = package_info["dist-tags"]["latest"]
@@ -30,26 +36,34 @@ class NPMMetadataMismatch(Detector):
         if path is None:
             raise ValueError("path is needed to run heuristic " + self.get_name())
         package_json = Path(path) / "package" / "package.json"
-        package_manifest: dict[str, Any] = json.loads(package_json.read_text())
+        package_manifest: Dict[str, Any] = json.loads(package_json.read_text())
 
         # Get NPM manifest for version
         version_info = package_info["versions"][version]
 
-        diff: dict[str, Diff] = {
+        diff = {
             field: difference_at_key(version_info, package_manifest, field, field_type)
             for field, field_type in MANIFEST_FIELDS_CHECKLIST.items()
         }
+
         number_different = sum(len(v) for v in diff.values())
-        diff_description = describe_diff(diff) if number_different != 0 else "No differences found"
+
+        diff_description = (
+            describe_diff(diff) if number_different != 0 else "No differences found"
+        )
+
         return number_different != 0, diff_description
 
 
 # PerItemDiff is (key, left_value, right_value)
-PerItemDiff = tuple[str, Optional[str], Optional[str]]
+PerItemDiff = tuple[str, Optional[Any], Optional[Any]]
 Diff = list[PerItemDiff]
 
 
-def diff_at_key_dict(version_at_key: dict[str, Optional[str]], manifest_at_key: dict[str, Optional[str]]) -> Diff:
+def diff_at_key_dict(
+    version_at_key: Dict[str, Optional[Any]],
+    manifest_at_key: Dict[str, Optional[Any]],
+) -> Diff:
     return [
         (key, version_at_key.get(key), manifest_at_key.get(key))
         for key in set(version_at_key.keys()).union(set(manifest_at_key.keys()))
@@ -57,19 +71,38 @@ def diff_at_key_dict(version_at_key: dict[str, Optional[str]], manifest_at_key: 
     ]
 
 
-def difference_at_key(version_info: dict[str, Any], package_manifest: dict[str, Any], key: str, key_type: type) -> Diff:
+def difference_at_key(
+    version_info: Dict[str, Any],
+    package_manifest: Dict[str, Any],
+    key: str,
+    key_type: type,
+) -> Diff:
     version_at_key = version_info.get(key, key_type())
     manifest_at_key = package_manifest.get(key, key_type())
-    if not (isinstance(version_at_key, key_type) and isinstance(manifest_at_key, key_type)):
-        return [(f"Expected type {str(key_type)}", f"{type(version_at_key)}", f"{type(manifest_at_key)}")]
+
+    if not (
+        isinstance(version_at_key, key_type) and isinstance(manifest_at_key, key_type)
+    ):
+        return [
+            (
+                f"Expected type {str(key_type)}",
+                f"{type(version_at_key)}",
+                f"{type(manifest_at_key)}",
+            )
+        ]
     elif key_type == dict:
-        return diff_at_key_dict(version_at_key, manifest_at_key)
+        return diff_at_key_dict(version_at_key, manifest_at_key)  # type: ignore
     else:
-        # If it is not a dict do a direct comparison of the value at the key, currently the only other type is strings
-        return [(f"{key}", version_at_key, manifest_at_key)] if version_at_key != manifest_at_key else []
+        # If it is not a dict do a direct comparison of the value at the key.
+        # Currently the only other type is strings.
+        return (
+            [(key, version_at_key, manifest_at_key)]
+            if version_at_key != manifest_at_key
+            else []
+        )
 
 
-def describe_diff(diff: dict[str, Diff]) -> str:
+def describe_diff(diff: Dict[str, Diff]) -> str:
     """
     Creates a string of the form
     Difference between manifest and package.json found:
@@ -90,7 +123,9 @@ def describe_diff(diff: dict[str, Diff]) -> str:
             field_description = f"{k}: \n"
             if MANIFEST_FIELDS_CHECKLIST[k] == dict:
                 for d in differences:
-                    field_description += f"  {d[0]}: Manifest(\"{d[1]}\"), package.json(\"{d[2]}\") \n"
+                    field_description += (
+                        f'  {d[0]}: Manifest("{d[1]}"), package.json("{d[2]}") \n'
+                    )
             else:
                 manifest_str = "  Manifest:\n"
                 package_str = "  package.json:\n"
