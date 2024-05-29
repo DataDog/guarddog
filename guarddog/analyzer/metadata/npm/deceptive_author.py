0f2ebbfd94b9e7afd6d47f4d283e559dc659e6ff
Sebastian Obregoso <sebastian.obregoso@datadoghq.com>
2024-05-29 15:23:17 +0200

    adding detection

---
+++ guarddog/analyzer/metadata/npm/deceptive_author.py
@@ -0,0 +1,13 @@
+""" Deceptive Author Detector
+
+Detects when an author of is using a disposable email
+"""
+
+from guarddog.analyzer.metadata.deceptive_author import DeceptiveAuthorDetector
+
+from .utils import get_email_addresses
+
+
+class NPMDeceptiveAuthor(DeceptiveAuthorDetector):
+    def get_email_addresses(self, package_info: dict):
+        return get_email_addresses(package_info)
