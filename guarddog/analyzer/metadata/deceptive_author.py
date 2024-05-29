dfad7b3f8970470c7e62df20132ce16702f3b13d
Sebastian Obregoso <sebastian.obregoso@datadoghq.com>
2024-05-29 15:32:40 +0200

    lint

--- guarddog/analyzer/metadata/deceptive_author.py
+++ guarddog/analyzer/metadata/deceptive_author.py
@@ -1,7 +1,7 @@
 from abc import abstractmethod
 from typing import Optional
 import os
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 
 import requests
 
@@ -9,7 +9,10 @@ from guarddog.analyzer.metadata.detector import Detector
 
 from .utils import extract_email_address_domain
 
-DISPOSABLE_EMAIL_DOMAINS_CACHE_LOCATION = os.environ.get('GUARDDOG_DISPOSABLE_EMAIL_DOMAINS_CACHE_LOCATION')
+DISPOSABLE_EMAIL_DOMAINS_CACHE_LOCATION = os.environ.get(
+    "GUARDDOG_DISPOSABLE_EMAIL_DOMAINS_CACHE_LOCATION"
+)
+
 
 class DeceptiveAuthorDetector(Detector):
     """This heuristic detects when an author is using a disposable email."""
@@ -19,34 +22,43 @@ class DeceptiveAuthorDetector(Detector):
     def __init__(self):
         super().__init__(
             name="deceptive_author",
-            description="This heuristic detects when an author is using a disposable email"
+            description="This heuristic detects when an author is using a disposable email",
         )
 
     def get_email_addresses(self, package_info: dict) -> set[str]:
         return set()
-    
+
     def _get_disposable_email_domains(self) -> list:
         """
         Gets the domains that are known to provide disposable e-mails
 
         """
 
-        disposable_email_domains_url = "https://raw.githubusercontent.com/wesbos/burner-email-providers/master/emails.txt"
+        disposable_email_domains_url = \
+            "https://raw.githubusercontent.com/wesbos/burner-email-providers/master/emails.txt"
 
         disposable_email_domains_filename = "disposable_email_domains.txt"
         resources_dir = DISPOSABLE_EMAIL_DOMAINS_CACHE_LOCATION
         if resources_dir is None:
-            resources_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "resources"))
+            resources_dir = os.path.abspath(
+                os.path.join(os.path.dirname(__file__), "resources")
+            )
 
-        disposable_email_domains_path = os.path.join(resources_dir, disposable_email_domains_filename)
+        disposable_email_domains_path = os.path.join(
+            resources_dir, disposable_email_domains_filename
+        )
 
         disposable_email_domains_data = None
 
         if disposable_email_domains_filename in os.listdir(resources_dir):
-            update_time = datetime.fromtimestamp(os.path.getmtime(disposable_email_domains_path))
+            update_time = datetime.fromtimestamp(
+                os.path.getmtime(disposable_email_domains_path)
+            )
 
             if datetime.now() - update_time <= timedelta(days=30):
-                with open(disposable_email_domains_path, "r") as disposable_email_domains_file:
+                with open(
+                    disposable_email_domains_path, "r"
+                ) as disposable_email_domains_file:
                     disposable_email_domains_data = disposable_email_domains_file.read()
 
         if disposable_email_domains_data is None:
@@ -59,8 +71,13 @@ class DeceptiveAuthorDetector(Detector):
         return disposable_email_domains_data.split("\n")
 
     @abstractmethod
-    def detect(self, package_info, path: Optional[str] = None, name: Optional[str] = None,
-               version: Optional[str] = None) -> tuple[bool, str]:
+    def detect(
+        self,
+        package_info,
+        path: Optional[str] = None,
+        name: Optional[str] = None,
+        version: Optional[str] = None,
+    ) -> tuple[bool, str]:
         emails = self.get_email_addresses(package_info)
 
         if len(emails) == 0:
@@ -73,4 +90,3 @@ class DeceptiveAuthorDetector(Detector):
             if domain in self._get_disposable_email_domains():
                 return True, self.MESSAGE_TEMPLATE % email
         return False, ""
-                
