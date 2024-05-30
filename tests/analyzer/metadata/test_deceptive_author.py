a72115638b36477f66ce4679a646e975c53e3445
Sebastian Obregoso <sebastian.obregoso@datadoghq.com>
2024-05-30 18:38:48 +0200

    finished test cases

--- tests/analyzer/metadata/test_deceptive_author.py
+++ tests/analyzer/metadata/test_deceptive_author.py
@@ -7,19 +7,15 @@ import guarddog.analyzer.metadata.utils
 from guarddog.analyzer.metadata.npm.deceptive_author import NPMDeceptiveAuthor
 from guarddog.analyzer.metadata.pypi.deceptive_author import PypiDeceptiveAuthor
 from tests.analyzer.metadata.resources.sample_project_info import (
-    PYPI_PACKAGE_INFO,
     generate_pypi_project_info,
-    generate_npm_project_info
-    )
+    generate_npm_project_info,
+)
 
 from tests.analyzer.metadata.utils import MockWhoIs
 
+pypi_detector = PypiDeceptiveAuthor()
+npm_detector = NPMDeceptiveAuthor()
 
-with open(os.path.join(pathlib.Path(__file__).parent.resolve(), "resources", "npm_data.json"), "r") as file:
-    NPM_PACKAGE_INFO = json.load(file)
-
-pypi_detector = NPMDeceptiveAuthor()
-npm_detector = PypiDeceptiveAuthor()
 
 # required because mocking in tests will cause get_domain_creation_date()
 # to return different results for a same domain
@@ -27,23 +23,40 @@ npm_detector = PypiDeceptiveAuthor()
 def clear_caches():
     guarddog.analyzer.metadata.utils.get_domain_creation_date.cache_clear()
 
+
 class TestDeceptiveAuthor:
 
-    disposable_author_pypi = generate_pypi_project_info("author_email", "john@example.com")
-    disposable_author_npm = generate_npm_project_info("versions",{"1.1.1.1":{"maintainters":[{"name":"john doe","email":"john@example.com"}]}})
-    non_disposable_author_pypi = generate_pypi_project_info("author_email", "some@gmail.com")
-    non_disposable_author_pypi = generate_pypi_project_info("author_email", "some@gmail.com")
+    disposable_author_pypi = generate_pypi_project_info(
+        "author_email", "john@example.com"
+    )
+    disposable_author_npm = generate_npm_project_info(
+        "maintainers", [{"name": "john doe", "email": "john@example.com"}]
+    )
+    non_disposable_author_pypi = generate_pypi_project_info(
+        "author_email", "john@gmail.com"
+    )
+    non_disposable_author_npm = generate_npm_project_info(
+        "maintainers", [{"name": "john doe", "email": "john@gmail.com"}]
+    )
 
-
-    @pytest.mark.parametrize("package_info, detector",
-                             [(disposable_author_pypi, pypi_detector), (disposable_author_npm, npm_detector)])
+    @pytest.mark.parametrize(
+        "package_info, detector",
+        [
+            (disposable_author_pypi, pypi_detector),
+            (disposable_author_npm, npm_detector),
+        ],
+    )
     def test_disposable_email(self, package_info, detector):
-        compromised, _ = detector.detect(package_info)
+        compromised, x = detector.detect(package_info)
         assert compromised
 
-    @pytest.mark.parametrize("package_info, detector",
-                             [(non_disposable_author_pypi, pypi_detector), (non_disposable_author_pypi, npm_detector)])
+    @pytest.mark.parametrize(
+        "package_info, detector",
+        [
+            (non_disposable_author_pypi, pypi_detector),
+            (non_disposable_author_npm, npm_detector),
+        ],
+    )
     def test_non_disposable_email(self, package_info, detector):
         compromised, _ = detector.detect(package_info)
         assert not compromised
-
