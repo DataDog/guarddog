0d21a0033a5ed85bfdc3f6fb6eb82b5d51e0d2dd
Sebastian Obregoso <sebastian.obregoso@datadoghq.com>
2024-07-04 11:00:34 +0200

    fix filtering, added tests and types to rule filtering

--- tests/core/test_cli.py
+++ tests/core/test_cli.py
@@ -1,5 +1,6 @@
 import os
 import unittest.mock
+from guarddog.ecosystems import ECOSYSTEM
 
 import guarddog.cli
 
@@ -18,4 +19,16 @@ def test_is_local_target():
     with unittest.mock.patch('os.path.exists') as mock:
         mock.return_value = False
         assert not guarddog.cli.is_local_target("foo.tar.gz")
+
+
+def test_get_rule_param_include():
+    assert len(guarddog.cli._get_rule_param(("shady-links",), (), ECOSYSTEM.NPM)) == 1
+
+def test_get_rule_param_exclude():
+    rules = guarddog.cli._get_rule_param((), ("shady-links",), ECOSYSTEM.PYPI)
+    assert len(rules) != 1
+    assert "shady-links" not in rules
+
+
+
 
