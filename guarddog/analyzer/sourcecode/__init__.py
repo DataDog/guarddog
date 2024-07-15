64b08693f26090f159ffb5977cead85175ea9d18
Sebastian Obregoso <sebastian.obregoso@datadoghq.com>
2024-07-15 17:59:38 +0200

    added sarif tests and reworked the SOURCECODE_RULES variable concept

--- guarddog/analyzer/sourcecode/__init__.py
+++ guarddog/analyzer/sourcecode/__init__.py
@@ -1,5 +1,7 @@
 import os
 import pathlib
+from dataclasses import dataclass
+from typing import Optional
 
 import yaml
 from yaml.loader import SafeLoader
@@ -7,18 +9,43 @@ from yaml.loader import SafeLoader
 from guarddog.ecosystems import ECOSYSTEM
 
 current_dir = pathlib.Path(__file__).parent.resolve()
+
+
+# These data class aim to reduce the spreading of the logic
+# Instead of using the a dict as a structure and parse it difffently depending on the type
+@dataclass
+class SourceCodeRule:
+    """
+    Base class for source code rules
+    """
+    id: str
+    file: str
+
+
+@dataclass
+class YaraRule(SourceCodeRule):
+    """
+    Yara rule just reimplements base
+    """
+    pass
+
+
+@dataclass
+class SempgrepRule(SourceCodeRule):
+    """
+    Semgrep rule are language specific
+    Content of rule in yaml format is accessible through rule_content
+    """
+    description: str
+    ecosystem: ECOSYSTEM
+    rule_content: dict
+
+
+SOURCECODE_RULES: list[SourceCodeRule] = list()
+
 semgrep_rule_file_names = list(
-    filter(
-        lambda x: x.endswith('yml'),
-        os.listdir(current_dir)
-    )
+    filter(lambda x: x.endswith("yml"), os.listdir(current_dir))
 )
-
-SEMGREP_SOURCECODE_RULES = {
-    ECOSYSTEM.PYPI: list(),
-    ECOSYSTEM.NPM: list(),
-}  # type: dict[ECOSYSTEM, list[dict]]
-
 # all yml files placed in the sourcecode directory are loaded as semgrep rules
 # refer to README.md for more information
 for file_name in semgrep_rule_file_names:
@@ -26,28 +53,48 @@ for file_name in semgrep_rule_file_names:
         data = yaml.load(fd, Loader=SafeLoader)
         for rule in data["rules"]:
             for lang in rule["languages"]:
+                ecosystem = None
                 match lang:
                     case "python":
-                        if rule not in SEMGREP_SOURCECODE_RULES[ECOSYSTEM.PYPI]:
-                            SEMGREP_SOURCECODE_RULES[ECOSYSTEM.PYPI].append(rule)
+                        ecosystem = ECOSYSTEM.PYPI
                     case "javascript" | "typescript" | "json":
-                        if rule not in SEMGREP_SOURCECODE_RULES[ECOSYSTEM.NPM]:
-                            SEMGREP_SOURCECODE_RULES[ECOSYSTEM.NPM].append(rule)
+                        ecosystem = ECOSYSTEM.NPM
+                    case _:
+                        continue
+
+                # avoids duplicates when multiple languages are supported by a rule
+                if not [r for r in SOURCECODE_RULES if r.id == rule["id"]]:
+                    SOURCECODE_RULES.append(
+                        SempgrepRule(
+                            id=rule["id"],
+                            ecosystem=ecosystem,
+                            description=rule.get("metadata", {}).get("description", ""),
+                            file=file_name,
+                            rule_content=rule,
+                        )
+                    )
 
 yara_rule_file_names = list(
-    filter(
-        lambda x: x.endswith('yar'),
-        os.listdir(current_dir)
-    )
+    filter(lambda x: x.endswith("yar"), os.listdir(current_dir))
 )
-
-YARA_SOURCECODE_RULES: set[str] = set()
-
 # all yar files placed in the sourcecode directory are loaded as YARA rules
 # refer to README.md for more information
 for file_name in yara_rule_file_names:
-    YARA_SOURCECODE_RULES.add(pathlib.Path(file_name).stem)
+    SOURCECODE_RULES.append(YaraRule(id=pathlib.Path(file_name).stem, file=file_name))
 
 
-def get_sourcecode_rules(ecosystem: ECOSYSTEM) -> set:
-    return {r["id"] for r in SEMGREP_SOURCECODE_RULES[ecosystem]} | YARA_SOURCECODE_RULES
+def get_sourcecode_rules(
+    ecosystem: ECOSYSTEM, kind: Optional[type] = None
+) -> list[SourceCodeRule]:
+    """
+    This function returns the source code rules for a given ecosystem and kind.
+    Args:
+        ecosystem: The ecosystem to filter for if rules are ecosystem specific
+        kind: The kind of rule to filter for
+    """
+    return [
+        rule
+        for rule in SOURCECODE_RULES
+        if (getattr(rule, "ecosystem", ecosystem) == ecosystem)
+        and (not kind or isinstance(rule, kind))
+    ]
