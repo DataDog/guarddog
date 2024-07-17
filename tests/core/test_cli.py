de09a503e74fd084a31046217cfb617c6fdef44e
Ian Kretz <44385082+ikretz@users.noreply.github.com>
2024-07-17 12:07:30 +0200

    Consolidate archive extension checking

--- tests/core/test_cli.py
+++ tests/core/test_cli.py
@@ -5,17 +5,39 @@ from guarddog.ecosystems import ECOSYSTEM
 
 
 def test_is_local_target():
-    assert guarddog.cli.is_local_target("/tmp/foo")
-    assert guarddog.cli.is_local_target("./foo")
-    assert guarddog.cli.is_local_target("../foo")
+    with unittest.mock.patch("os.path.isdir") as mock:
+        mock.return_value = True
+        assert guarddog.cli.is_local_target("/tmp/foo")
+
+    with unittest.mock.patch("os.path.isdir") as mock:
+        mock.return_value = False
+        assert not guarddog.cli.is_local_target("/tmp/foo")
+
+    with unittest.mock.patch("os.path.isdir") as mock:
+        mock.return_value = True
+        assert guarddog.cli.is_local_target("./foo")
+
+    with unittest.mock.patch("os.path.isdir") as mock:
+        mock.return_value = False
+        assert not guarddog.cli.is_local_target("./foo")
+
+    with unittest.mock.patch("os.path.isdir") as mock:
+        mock.return_value = True
+        assert guarddog.cli.is_local_target("../foo")
+
+    with unittest.mock.patch("os.path.isdir") as mock:
+        mock.return_value = False
+        assert not guarddog.cli.is_local_target("../foo")
+
     assert guarddog.cli.is_local_target(".")
+
     assert not guarddog.cli.is_local_target("foo")
 
-    with unittest.mock.patch("os.path.exists") as mock:
+    with unittest.mock.patch("os.path.isfile") as mock:
         mock.return_value = True
         assert guarddog.cli.is_local_target("foo.tar.gz")
 
-    with unittest.mock.patch("os.path.exists") as mock:
+    with unittest.mock.patch("os.path.isfile") as mock:
         mock.return_value = False
         assert not guarddog.cli.is_local_target("foo.tar.gz")
 
