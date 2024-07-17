e868288fee2f3c7af4d4560595013aaf4f23daef
Julien Doutre <julien.doutre@datadoghq.com>
2024-07-17 16:41:08 +0200

    Use data classes and remove dead code

--- guarddog/scanners/go_project_scanner.py
+++ guarddog/scanners/go_project_scanner.py
@@ -1,28 +1,25 @@
 import logging
-import requests
+from dataclasses import dataclass
+from typing import List
 
-from guarddog.scanners.go_package_scanner import (
-    GoModuleScanner,
-    GOPROXY_URL,
-    escape_module_name,
-)
+from guarddog.scanners.go_package_scanner import GoModuleScanner
 from guarddog.scanners.scanner import ProjectScanner
 
 log = logging.getLogger("guarddog")
 
 
+@dataclass
+class GoRequirement:
+    module: str
+    version: str
+
+
+@dataclass
 class GoModule:
-    def __init__(self):
-        self.module = ""
-        self.go = ""
-        self.toolchain = ""
-        self.requirements = []
-
-
-class Requirement:
-    def __init__(self, module: str, version: str):
-        self.module = module
-        self.version = version
+    module: str
+    go: str
+    toolchain: str
+    requirements: List[GoRequirement]
 
 
 class GoDependenciesScanner(ProjectScanner):
@@ -31,8 +28,6 @@ class GoDependenciesScanner(ProjectScanner):
 
     def parse_requirements(self, raw_requirements: str) -> dict[str, set[str]]:
         main_mod = self.parse_go_mod_file(raw_requirements)
-        # TODO: implement the Minimal Version Selection algorithm as described
-        # In https://research.swtch.com/vgo-mvs and https://go.dev/ref/mod#minimal-version-selection
 
         return {
             requirement.module: set([requirement.version])
@@ -41,7 +36,10 @@ class GoDependenciesScanner(ProjectScanner):
 
     # Read https://go.dev/ref/mod#go-mod-file to learn more about the go.mod syntax
     def parse_go_mod_file(self, go_mod_content: str) -> GoModule:
-        go_module = GoModule()
+        module = ""
+        go = ""
+        toolchain = ""
+        requirements = []
 
         is_in_block = None
         for line in go_mod_content.splitlines():
@@ -50,29 +48,21 @@ class GoDependenciesScanner(ProjectScanner):
             if line.startswith("//"):  # Ignore comments
                 continue
             elif line.startswith("module "):
-                go_module.module = line.split()[1]
+                module = line.split()[1]
             elif line.startswith("go "):
-                go_module.go = line.split()[1]
+                go = line.split()[1]
             elif line.startswith("toolchain "):
-                go_module.toolchain = line.split()[1]
+                toolchain = line.split()[1]
             elif line.startswith("require ("):
                 is_in_block = "require"
             elif line.startswith("require "):
                 parts = line.split()
-                go_module.requirements.append(Requirement(parts[1], parts[2]))
+                requirements.append(GoRequirement(parts[1], parts[2]))
             elif line.endswith(")") and is_in_block:
                 is_in_block = None
             elif is_in_block == "require" and line != "":
                 parts = line.split()
-                go_module.requirements.append(Requirement(parts[0], parts[1]))
+                requirements.append(GoRequirement(parts[0], parts[1]))
             # TODO: support exclude, replace and retract statements
 
-        return go_module
-
-    def get_go_mod_file(self, package_name: str, version: str) -> GoModule:
-        go_mod_file_url = (
-            f"{GOPROXY_URL}/{escape_module_name(package_name)}/@v/{version}.mod"
-        )
-        response = requests.get(go_mod_file_url)
-        response.raise_for_status()
-        return self.parse_go_mod_file(response.text)
+        return GoModule(module, go, toolchain, requirements)
