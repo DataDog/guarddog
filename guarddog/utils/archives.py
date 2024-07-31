2de0f93b1d7fc8e7baaf730a55686afaa7321990
Ian Kretz <44385082+ikretz@users.noreply.github.com>
2024-07-31 12:10:08 +0200

    Allow more tar archive compression algorithms (#430)

--- guarddog/utils/archives.py
+++ guarddog/utils/archives.py
@@ -1,7 +1,8 @@
 import logging
 import os
-import zipfile
 import stat
+import zipfile
+
 import tarsafe  # type:ignore
 
 log = logging.getLogger("guarddog")
@@ -9,41 +10,25 @@ log = logging.getLogger("guarddog")
 
 def is_supported_archive(path: str) -> bool:
     """
-    Decide whether a file contains a supported archive.
+    Decide whether a file contains a supported archive based on its
+    file extension.
 
     Args:
         path (str): The local filesystem path to examine
 
     Returns:
         bool: Represents the decision reached for the file
+
     """
+    def is_tar_archive(path: str) -> bool:
+        tar_exts = [".bz2", ".bzip2", ".gz", ".gzip", ".tgz", ".xz"]
+
+        return any(path.endswith(ext) for ext in tar_exts)
+
+    def is_zip_archive(path: str) -> bool:
+        return any(path.endswith(ext) for ext in [".zip", ".whl", ".egg"])
+
     return is_tar_archive(path) or is_zip_archive(path)
-
-
-def is_tar_archive(path: str) -> bool:
-    """
-    Decide whether a file contains a tar archive.
-
-    Args:
-        path (str): The local filesystem path to examine
-
-    Returns:
-        bool: Represents the decision reached for the file
-    """
-    return any(path.endswith(ext) for ext in [".tar.gz", ".tgz"])
-
-
-def is_zip_archive(path: str) -> bool:
-    """
-    Decide whether a file contains a zip, whl or egg archive.
-
-    Args:
-        path (str): The local filesystem path to examine
-
-    Returns:
-        bool: Represents the decision reached for the file
-    """
-    return any(path.endswith(ext) for ext in [".zip", ".whl", ".egg"])
 
 
 def safe_extract(source_archive: str, target_directory: str) -> None:
@@ -61,7 +46,7 @@ def safe_extract(source_archive: str, target_directory: str) -> None:
 
     """
     log.debug(f"Extracting archive {source_archive} to directory {target_directory}")
-    if is_tar_archive(source_archive):
+    if tarsafe.is_tarfile(source_archive):
 
         def add_exec(path):
             st = os.stat(path)
@@ -82,7 +67,7 @@ def safe_extract(source_archive: str, target_directory: str) -> None:
         tarsafe.open(source_archive).extractall(target_directory)
         recurse_add_perms(target_directory)
 
-    elif is_zip_archive(source_archive):
+    elif zipfile.is_zipfile(source_archive):
         with zipfile.ZipFile(source_archive, 'r') as zip:
             for file in zip.namelist():
                 # Note: zip.extract cleans up any malicious file name
