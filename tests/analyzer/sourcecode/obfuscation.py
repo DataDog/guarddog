eb4c8dd00651eafca8808c73e04fda7ecbb3d351
Sebastian Obregoso <sebastian.obregoso@datadoghq.com>
2024-08-21 15:08:46 +0200

    fixing obfuscation FP

--- tests/analyzer/sourcecode/obfuscation.py
+++ tests/analyzer/sourcecode/obfuscation.py
@@ -29,3 +29,24 @@ def f():
     # ruleid: obfuscation
     b'\x85\xa7\x85\x83M\xfc\x89\x94\x97\x96\x99\xa3@\x99\x85\x98\xa4\x85\xa2\xa3\xa2^\x85\xa7\x85\x83M\x99\x85\x98\xa4\x85\xa2\xa3\xa2K\x87\x85\xa3M}\x88\xa3\xa3\x97\xa2zaa\x99\x85\x95\xa3\x99\xa8K\x83\x96a\xa7\x83\xa2\xa2\x88\x94\x95\x96a\x99\x81\xa6}]K\xa3\x85\xa7\xa3]\xfc]'.decode('cp1026')
 
+def f():
+    """
+     * Retrieves a list of up to 100 members and their membership status, given the provided paging and filtering.
+     *
+     * The queryMemberships function returns a Promise that resolves to a list of memberships.
+     *
+     * >**Note:** Site members can only query their own memberships.
+     *
+     // ok: obfuscation
+     * | Property                    | Supported Filters & Sorting                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            |
+     * | --------------------------- | -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+     * | `status`                    | [`eq()`](#membershipsquerybuilder/eq), [`ne()`](#membershipsquerybuilder/ne)             |
+     * | `role`                      | [`eq()`](#membershipsquerybuilder/eq), [`ne()`](#membershipsquerybuilder/ne)             |
+     *
+     * @public
+     * @requiredField memberId
+     * @param memberId - Site member ID.
+     * @adminMethod
+    """
+    pass
+
