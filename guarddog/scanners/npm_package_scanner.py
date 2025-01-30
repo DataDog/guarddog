2e78d77d7b1b71072197410f037ac9a494d3e78c
Julien Doutre <julien.doutre@datadoghq.com>
2025-01-30 11:31:03 +0100

    Support scanning github actions

--- guarddog/scanners/npm_package_scanner.py
+++ guarddog/scanners/npm_package_scanner.py
@@ -18,18 +18,41 @@ class NPMPackageScanner(PackageScanner):
         super().__init__(Analyzer(ECOSYSTEM.NPM))
 
     def download_and_get_package_info(self, directory: str, package_name: str, version=None) -> typing.Tuple[dict, str]:
-        git_target = None
-        if urlparse(package_name).hostname is not None and package_name.endswith('.git'):
-            git_target = package_name
+        git_package_name = self._parse_git_url(package_name)
 
-        if not package_name.startswith("@") and package_name.count("/") == 1:
-            git_target = f"https://github.com/{package_name}.git"
+        if git_package_name != "":
+            data, tarball_url = self._get_git_data_and_tarball_url(git_package_name, version)
+        else:
+            data, tarball_url = self._get_npm_data_and_tarball_url(package_name, version)
 
-        if git_target is not None:
-            raise Exception("Git targets are not yet supported for npm")
+        log.debug(f"Downloading NPM package from {tarball_url}")
+        file_extension = pathlib.Path(tarball_url).suffix
+        zippath = os.path.join(directory, package_name.replace("/", "-") + file_extension)
+        unzippedpath = zippath.removesuffix(file_extension)
+        self.download_compressed(tarball_url, zippath, unzippedpath)
 
+        return data, unzippedpath
+
+    def _parse_git_url(self, package_name: str) -> str:
+        parsed_url = urlparse(package_name)
+
+        path = parsed_url.path.strip("/") # trim leading and trailing slashes
+
+        # TODO: support other git providers?
+        if parsed_url.hostname == "github.com" and path.endswith('.git') and not path.startswith("@") and path.count("/") == 1:
+            return path.removesuffix(".git")
+
+        return ""
+
+    def _get_git_data_and_tarball_url(self, package_name: str, version=None) -> typing.Tuple[dict, str]:
+        if version is None:
+            return {}, f"https://github.com/{package_name}/archive/refs/heads/main.zip"
+        else:
+            return {}, f"https://github.com/{package_name}/archive/refs/tags/{version}.zip"
+
+    def _get_npm_data_and_tarball_url(self, package_name: str, version=None) -> typing.Tuple[dict, str]:
         url = f"https://registry.npmjs.org/{package_name}"
-        log.debug(f"Downloading NPM package from {url}")
+        log.debug(f"Downloading NPM package metadata from {url}")
         response = requests.get(url)
 
         if response.status_code != 200:
@@ -37,16 +60,9 @@ class NPMPackageScanner(PackageScanner):
         data = response.json()
         if "name" not in data:
             raise Exception(f"Error retrieving package: {package_name}")
+
         # if version is none, we only scan the last package
         # TODO: figure logs and log it when we do that
         version = data["dist-tags"]["latest"] if version is None else version
 
-        details = data["versions"][version]
-
-        tarball_url = details["dist"]["tarball"]
-        file_extension = pathlib.Path(tarball_url).suffix
-        zippath = os.path.join(directory, package_name.replace("/", "-") + file_extension)
-        unzippedpath = zippath.removesuffix(file_extension)
-        self.download_compressed(tarball_url, zippath, unzippedpath)
-
-        return data, unzippedpath
+        return data, data["versions"][version]["dist"]["tarball"]
