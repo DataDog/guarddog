ae7a8bba236b126845e7f01e51e1270942573194
Julien Doutre <julien.doutre@datadoghq.com>
2025-02-05 12:05:39 +0100

    Use actual action in tests

--- tests/core/test_github_action_scanner.py
+++ tests/core/test_github_action_scanner.py
@@ -9,14 +9,14 @@ from guarddog.scanners import GithubActionScanner
 def test_download_and_get_github_action_by_url():
     scanner = GithubActionScanner()
     with tempfile.TemporaryDirectory() as tmpdirname:
-        data, path = scanner.download_and_get_package_info(tmpdirname, "https://github.com/expressjs/express.git", "v5.0.0")
+        data, path = scanner.download_and_get_package_info(tmpdirname, "https://github.com/actions/checkout.git", "v4.2.2")
         assert not data
-        assert os.path.exists(os.path.join(tmpdirname, "https:--github.com-expressjs-express.git", "express-5.0.0", "package.json"))
+        assert os.path.exists(os.path.join(tmpdirname, "https:--github.com-actions-checkout.git", "checkout-4.2.2", "package.json"))
 
 
 def test_download_and_get_github_action_by_name():
     scanner = GithubActionScanner()
     with tempfile.TemporaryDirectory() as tmpdirname:
-        data, path = scanner.download_and_get_package_info(tmpdirname, "expressjs/express", "v5.0.0")
+        data, path = scanner.download_and_get_package_info(tmpdirname, "actions/checkout", "v4.2.2")
         assert not data
-        assert os.path.exists(os.path.join(tmpdirname, "expressjs-express", "express-5.0.0", "package.json"))
+        assert os.path.exists(os.path.join(tmpdirname, "actions-checkout", "checkout-4.2.2", "package.json"))
