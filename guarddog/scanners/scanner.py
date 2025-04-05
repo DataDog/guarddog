a5d8686f76b7a7e8ae0a25861b7c6fe246f05a2e
Ian Kretz <44385082+ikretz@users.noreply.github.com>
2025-04-05 12:33:09 +0200

    Add missing cleanup in error exit

--- guarddog/scanners/scanner.py
+++ guarddog/scanners/scanner.py
@@ -357,25 +357,28 @@ class PackageScanner:
                 new_version_dir.name, name, new_version
             )
         except Exception as e:
+            old_version_dir.cleanup()
+            new_version_dir.cleanup()
             log.debug("Unable to download package, ignoring: " + str(e))
             return {"issues": 0, "errors": {"download-package": str(e)}}
 
         diff = DirectoryDiff.from_directories(Path(old_version_path), Path(new_version_path))
+        diff_dir = tempfile.TemporaryDirectory()
 
-        with tempfile.TemporaryDirectory() as diff_dir:
-            for path in diff.added:
-                src_path = diff.right / path
-                dst_path = Path(diff_dir) / path
-                if src_path.is_dir():
-                    shutil.copytree(src_path, dst_path, dirs_exist_ok=True)
-                if src_path.is_file():
-                    os.makedirs(dst_path.parent, exist_ok=True)
-                    shutil.copy(src_path, dst_path)
+        for path in diff.added:
+            src_path = diff.right / path
+            dst_path = Path(diff_dir.name) / path
+            if src_path.is_dir():
+                shutil.copytree(src_path, dst_path, dirs_exist_ok=True)
+            if src_path.is_file():
+                os.makedirs(dst_path.parent, exist_ok=True)
+                shutil.copy(src_path, dst_path)
 
-            results = self.analyzer.analyze_sourcecode(diff_dir, rules=rules)
+        results = self.analyzer.analyze_sourcecode(diff_dir.name, rules=rules)
 
         old_version_dir.cleanup()
         new_version_dir.cleanup()
+        diff_dir.cleanup()
 
         return results
 
