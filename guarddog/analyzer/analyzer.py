4172c7b9befd71fd4608127e1fefb4caa7f2ae35
Tesnim Mohamed Nejib Hamdouni <tesnim.hamdouni@datadoghq.com>
2025-04-11 13:38:09 +0200

    Semgrep timeout fix + use of env variables to set max_target_bytes

--- guarddog/analyzer/analyzer.py
+++ guarddog/analyzer/analyzer.py
@@ -15,7 +15,7 @@ from guarddog.ecosystems import ECOSYSTEM
 
 SEMGREP_MAX_TARGET_BYTES = int(
     os.getenv("GUARDDOG_SEMGREP_MAX_TARGET_BYTES", 10_000_000)
-)  # default to 10MB if no env variable found 
+)  # default to 10MB if no env variable found
 SEMGREP_TIMEOUT = 10  # maximum time to spend running a rule on a single file in seconds.
 SOURCECODE_RULES_PATH = os.path.join(
     os.path.dirname(__file__), "sourcecode"
